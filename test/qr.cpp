d859c3c5993e5fc0d3c082626c77e88ec4693856
wawando <muhammad.ridwan.ab@gmail.com>
2021-04-07 16:24:36 +0900

    Added qr_test file

---
+++ test/qr.cpp
@@ -0,0 +1,32 @@
+#include "Hatrix/Hatrix.h"
+
+#include <algorithm>
+#include <iostream>
+
+using namespace Hatrix;
+
+int main() {
+  //Full QR
+  Matrix A(8, 4), Q(8, 4), R(4, 4);
+  Matrix A_copy(8, 4);
+  Matrix QR(8,4);
+  QR = 0.0;
+  Q = 0.0;
+  R = 0.0;
+  qr(A, Q, R);
+  gemm(Q, R, QR);
+  
+  bool correct = true;
+  // Check result
+  for (int i=0; i<QR.rows; i++) {
+    for (int j=0; j<QR.cols; j++) {
+      if (std::abs(QR(i, j) - A_copy(i, j)) > 10e-8) {
+        correct = false;
+        std::cout << i << " " << j << ": ";
+        std::cout << QR(i, j) << " vs " << A_copy(i, j) << "\n";
+      }
+      break;
+    }
+  }
+  return correct ? 0 : 1;
+}
