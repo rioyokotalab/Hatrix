1653f0cb347068f3910f1118e1ddbf992aa49eb3
delta-leader <a0855909@unet.univie.ac.at>
2021-04-28 17:38:23 +0900

    Added Matrix creation from CSV

--- src/util/matrix_generators.cpp
+++ src/util/matrix_generators.cpp
@@ -4,6 +4,11 @@
 #include <cstdint>
 using std::int64_t;
 #include <random>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <fstream>
+#include <stdexcept>
 
 
 namespace Hatrix {
@@ -38,6 +43,45 @@ Matrix generate_identity_matrix(int64_t rows, int64_t cols) {
   Matrix out(rows, cols);
   for (int64_t i=0; i<out.min_dim(); ++i) {
     out(i, i) = 1.;
+  }
+  return out;
+}
+
+Matrix generate_from_csv(std::string filename, char delimiter, int64_t rows, int64_t cols) {
+  std::vector<std::vector<double>> data;
+  std::ifstream datafile(filename);
+  if (!datafile.is_open())
+    throw std::runtime_error("Could not open file \'"+filename+"\'");
+
+  std::string line;
+  double val;
+  int64_t len, n = 0;
+  while(std::getline(datafile, line)){
+    std::stringstream ss(line);
+    std::vector<double> row;
+    while(ss >> val){
+      row.push_back(val);
+      if(ss.peek() == delimiter)
+        ss.ignore();
+    }
+    len = row.size();
+    if (len > n)
+      n = len;
+    data.push_back(row);
+  }
+  datafile.close();
+
+  int64_t m = data.size();
+
+  Matrix out(rows > 0 ? rows : m, cols > 0 ? cols : n);
+  rows = out.rows < m ? out.rows : m;
+  //only fill values that are actually there
+  for (int64_t i=0; i<rows; ++i){
+    std::vector<double> row = data.at(i);
+    cols = row.size() < out.cols ? row.size() : out.cols;
+    for (int64_t j=0; j<cols; ++j){
+      out(i, j) = row.at(j);
+    }
   }
   return out;
 }
