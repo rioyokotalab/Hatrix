d0ee2444263cb70bb6c0763b599aad23e22964b2
delta-leader <a0855909@unet.univie.ac.at>
2021-05-02 19:53:11 +0900

    Fixed LUP to match P*A = LU

--- test/lu.cpp
+++ test/lu.cpp
@@ -41,24 +41,17 @@ TEST_P(LUTests, lup){
   std::tie(m, n) = GetParam();
 
   Hatrix::Matrix A = Hatrix::generate_random_matrix(m, n);
-
-  //set a large value on the diagonal to avoid pivoting
-  int64_t d = m * n;
   int64_t n_diag = A.min_dim();
-  for (int64_t i=0; i<n_diag; ++i){
-    A(i,i) += d--;
-  }
-
   Hatrix::Matrix A_copy(A);
   Hatrix::Matrix L(m, n_diag), U(n_diag, n), P(m, m), A_rebuilt(m, n);
   Hatrix::lup(A, L, U, P);
-  Hatrix::matmul(L, U, A, false, false, 1, 0);
-  Hatrix::matmul(P, A, A_rebuilt, false, false, 1, 0);
+  Hatrix::matmul(L, U, A_rebuilt, false, false, 1, 0);
+  Hatrix::matmul(P, A_copy, A, false, false, 1, 0);
 
     // Check result
   for (int64_t i=0; i<A.rows; ++i) {
     for (int64_t j=0; j<A.cols; ++j) {
-      EXPECT_FLOAT_EQ(A_rebuilt(i, j), A_copy(i, j));
+      EXPECT_FLOAT_EQ(A_rebuilt(i, j), A(i, j));
     }
   }
 }
