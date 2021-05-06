60e69d31687eeb928e8f49b7de02bf92b9211b57
delta-leader <a0855909@unet.univie.ac.at>
2021-05-06 15:58:35 +0900

    Added scalar multiplication operator

--- include/Hatrix/functions/arithmetics.h
+++ include/Hatrix/functions/arithmetics.h
@@ -11,6 +11,8 @@ Matrix& operator-=(Matrix& A, const Matrix& B);
 Matrix operator-(const Matrix& A, const Matrix& B);
 
 Matrix operator*(const Matrix& A, const Matrix& B);
+Matrix operator*(const Matrix& A, double alpha);
+Matrix operator*(double alpha, const Matrix& A);
 
 Matrix abs(const Matrix& A);
 
