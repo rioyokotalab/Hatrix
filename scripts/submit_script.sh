39fecf02134cf02bc350c03432743c49287b8320
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2021-05-07 16:47:38 +0900

    update ABCI modules

--- scripts/submit_script.sh
+++ scripts/submit_script.sh
@@ -6,6 +6,6 @@ cd Hatrix
 
 if [[ $(uname -n) = "login" ]]; then
     yrun scripts/SC_instructions/lab.sh
-elif [[ $(uname -n) = "es1.abci.local" ]]; then
+elif [[ $(uname -n) == *abci.local ]]; then
     qsub -g gcc50609 scripts/SC_instructions/abci.sh
 fi
