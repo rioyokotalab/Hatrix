df428a7eafc71b3d03ce6d0372f39d9757c2ee8b
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2021-05-07 16:31:55 +0900

    experiment with ABCI script

--- scripts/submit_script.sh
+++ scripts/submit_script.sh
@@ -1,14 +1,10 @@
-rm -rf $HOME/dev/sandbox
 mkdir -p $HOME/dev/sandbox
 cd $HOME/dev/sandbox
 git clone --depth 1 --branch dev git@github.com:rioyokotalab/Hatrix.git
 cd Hatrix
 
-echo `pwd`
-if [[ ${SYSTEM_NAME} = "YOKOTA_LAB" ]]; then
-    ybatch scripts/SC_instructions/lab.sh
-elif [[ ${SYSTEM_NAME} = "ABCI" ]]; then
+if [[ $(uname -n) = "login" ]]; then
+    yrun scripts/SC_instructions/lab.sh
+elif [[ $(uname -n) = "es1.abci.local" ]]; then
     qsub -g gcc50609 scripts/SC_instructions/abci.sh
-elif [[ ${SYSTEM_NAME} = "TSUBAME" ]]; then
-    qsub -g scripts/SC_instructions/tsubame.sh
 fi
