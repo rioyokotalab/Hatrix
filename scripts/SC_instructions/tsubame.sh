0cb64690f4b1621cc48a0b14ce37f09f7bf93ffd
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2021-05-07 17:10:05 +0900

    tsubame file

--- scripts/SC_instructions/tsubame.sh
+++ scripts/SC_instructions/tsubame.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
-
 #$ -cwd
-#$ -l rt_F=1
-#$ -l h_rt=01:00:00
-#$ -N HATRIX_TEST
-#$ -o HATRIX_TEST_out.log
-#$ -e HATRIX_TEST_err.log
+#$ -l f_node=1
+#$ -l h_rt=00:20:00
+#$ -N HATRIX_TSUBAME
+#$ -o HATRIX_TSUBAME_out.log
+#$ -e HATRIX_TSUBAME_err.log
 #$ -m ea
 #$ -M deshmukh.s.aa@m.titech.ac.jp
 
 set -e
 
 printf "#### Setting up environment... "
-source /etc/profile.d/modules.sh
-printf "Done\n"
-
 cd $HOME/dev/sandbox/Hatrix
+
 printf "#### Done\n"
 
 #################### CPU only build                         ####################
 printf "#### Building without CUDA... \n"
 # Necessary modules
-module load cmake/3.19 gcc intel-mkl
+. /etc/profile.d/modules.sh
+module load cmake/3.17.2 gcc/8.3.0
 
 # Build
 mkdir build
