d9283000d6a4b4970f49f2b16c7460cf8a8cc4b9
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2021-05-07 17:39:03 +0900

    remove pseudo

--- scripts/run_on_all_SCs.sh
+++ scripts/run_on_all_SCs.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-set -e
-
-for server in lab; do
-    ssh  $server 'bash -l -s' < submit_script.sh
+for server in lab abci; do
+    ssh $server 'bash -l -s' < submit_script.sh
 done
 
 echo "Submitted batch scripts for all machines!"
