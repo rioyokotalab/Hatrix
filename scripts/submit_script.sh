517ea68fb43f2e6d0c63f854c6bef9e71e3c7b45
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2021-05-07 16:33:45 +0900

    update branch namem

--- scripts/submit_script.sh
+++ scripts/submit_script.sh
@@ -1,6 +1,6 @@
 mkdir -p $HOME/dev/sandbox
 cd $HOME/dev/sandbox
-git clone --depth 1 --branch dev git@github.com:rioyokotalab/Hatrix.git
+git clone --depth 1 --branch feature/SC_automation git@github.com:rioyokotalab/Hatrix.git
 cd Hatrix
 
 if [[ $(uname -n) = "login" ]]; then
