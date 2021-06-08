f419b929fd4c0561388516411c8bc42e68315fa6
derpda <peter.spalthoff@gmail.com>
2021-06-08 10:46:54 +0900

    split functions with tests added

--- src/classes/Matrix.cpp
+++ src/classes/Matrix.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cassert>
 #include <cstdint>
+#include <cstddef>
 #include <iostream>
 #include <memory>
 #include <vector>
@@ -24,7 +25,7 @@ class Matrix::DataHandler {
 
   const double* get_ptr() const { return data_.data(); }
 
-  int64_t size() const { return data_.size(); }
+  size_t size() const { return data_.size(); }
 
   void resize(int64_t size) { data_.resize(size); }
 };
@@ -89,6 +90,59 @@ void Matrix::shrink(int64_t new_rows, int64_t new_cols) {
   data->resize(rows * cols);
 }
 
+std::vector<Matrix> Matrix::split(int64_t n_row_splits, int64_t n_col_splits,
+                                  bool copy) const {
+  int64_t row_split_size = rows / n_row_splits;
+  std::vector<int64_t> row_split_indices;
+  for (int64_t i=1; i<n_row_splits; ++i) {
+    row_split_indices.push_back(row_split_size*i);
+  }
+  int64_t col_split_size = cols / n_col_splits;
+  std::vector<int64_t> col_split_indices;
+  for (int64_t j=1; j<n_col_splits; ++j) {
+    col_split_indices.push_back(col_split_size*j);
+  }
+  return split(row_split_indices, col_split_indices, copy);
+}
+
+std::vector<Matrix> Matrix::split(const std::vector<int64_t>& row_split_indices,
+                                  const std::vector<int64_t>& col_split_indices,
+                                  bool copy) const {
+  std::vector<Matrix> parts;
+  auto row_iter = row_split_indices.cbegin();
+  int64_t row_start = 0;
+  while (row_start < rows) {
+    int64_t row_end = row_iter == row_split_indices.end() ? rows : *row_iter++;
+    int64_t n_rows = row_end - row_start;
+    auto col_iter = col_split_indices.cbegin();
+    int64_t col_start = 0;
+    while (col_start < cols) {
+      int64_t col_end =
+          col_iter == col_split_indices.end() ? cols : *col_iter++;
+      Matrix part_of_this;
+      if (copy) {
+        part_of_this = Matrix(n_rows, col_end - col_start);
+        double* part_start = &data_ptr[col_start * stride + row_start];
+        for (int64_t j = 0; j < part_of_this.cols; j++) {
+          for (int64_t i = 0; i < part_of_this.rows; i++) {
+            part_of_this(i, j) = part_start[j * stride + i];
+          }
+        }
+      } else {
+        part_of_this.rows = n_rows;
+        part_of_this.cols = col_end - col_start;
+        part_of_this.stride = stride;
+        part_of_this.data = std::make_shared<DataHandler>(*data);
+        part_of_this.data_ptr = &data_ptr[col_start * stride + row_start];
+      }
+      parts.emplace_back(std::move(part_of_this));
+      col_start = col_end;
+    }
+    row_start = row_end;
+  }
+  return parts;
+}
+
 int64_t Matrix::min_dim() const { return std::min(rows, cols); }
 int64_t Matrix::max_dim() const { return std::max(rows, cols); }
 
@@ -100,6 +154,12 @@ void Matrix::print() const {
     std::cout << "\n";
   }
   std::cout << "\n";
+}
+
+size_t Matrix::memory_used() const { return rows * cols * sizeof(double); }
+
+size_t Matrix::shared_memory_used() const {
+  return data->size() * sizeof(double);
 }
 
 }  // namespace Hatrix
--- test/Matrix.cpp
+++ test/Matrix.cpp
@@ -1,5 +1,6 @@
 #include <cstdint>
 #include <utility>
+#include <vector>
 
 #include "Hatrix/Hatrix.h"
 #include "gtest/gtest.h"
@@ -79,5 +80,75 @@ TEST(MatrixTests, shrink) {
     for (int64_t j = 0; j < shrunk_size; ++j) {
       EXPECT_EQ(A(i, j), A_shrunk(i, j));
     }
+  }
+}
+
+class MatrixTests : public testing::TestWithParam<
+                        std::tuple<int64_t, int64_t, int64_t, int64_t, bool>> {
+};
+
+TEST_P(MatrixTests, split) {
+  int64_t M, N;
+  int64_t m_splits, n_splits;
+  bool copy;
+  std::tie(M, N, m_splits, n_splits, copy) = GetParam();
+  Hatrix::Matrix A = Hatrix::generate_random_matrix(M, N);
+
+  std::vector<Hatrix::Matrix> split = A.split(m_splits, n_splits, copy);
+
+  // Check result
+  int64_t m_start = 0, n_start = 0;
+  for (int64_t i_c = 0; i_c < m_splits; ++i_c) {
+    n_start = 0;
+    for (int64_t j_c = 0; j_c < n_splits; ++j_c) {
+      Hatrix::Matrix& block = split[i_c * n_splits + j_c];
+      for (int64_t i = 0; i < block.rows; ++i) {
+        for (int64_t j = 0; j < block.cols; ++j) {
+          EXPECT_EQ(A(m_start + i, n_start + j), block(i, j));
+        }
+      }
+      if (copy) {
+        EXPECT_EQ(block.memory_used(), block.shared_memory_used());
+      } else {
+        EXPECT_EQ(block.shared_memory_used(), A.shared_memory_used());
+      }
+      n_start += block.cols;
+    }
+    m_start += split[i_c * n_splits].rows;
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(Sizes, MatrixTests,
+                         testing::Combine(testing::Values(32, 57),
+                                          testing::Values(32, 57),
+                                          testing::Values(1, 3, 8),
+                                          testing::Values(1, 3, 8),
+                                          testing::Values(true, false)));
+
+TEST(MatrixTests, shrinktest) {
+  Hatrix::Matrix A = Hatrix::generate_random_matrix(64, 64);
+  std::vector<int64_t> row_split_indices{6, 37, 55};
+  std::vector<int64_t> col_split_indices{25, 41, 49};
+
+  std::vector<Hatrix::Matrix> split =
+      A.split(row_split_indices, col_split_indices, false);
+
+  // Check result
+  int64_t m_start = 0, n_start = 0;
+  for (int64_t i_c = 0; i_c < row_split_indices.size() + 1; ++i_c) {
+    n_start = 0;
+    for (int64_t j_c = 0; j_c < col_split_indices.size() + 1; ++j_c) {
+      Hatrix::Matrix& block = split[i_c * (col_split_indices.size()+1) + j_c];
+      EXPECT_EQ(i_c == 0 ? 0 : row_split_indices[i_c-1], m_start);
+      EXPECT_EQ(j_c == 0 ? 0 : col_split_indices[j_c-1], n_start);
+      for (int64_t i = 0; i < block.rows; ++i) {
+        for (int64_t j = 0; j < block.cols; ++j) {
+          EXPECT_EQ(A(m_start + i, n_start + j), block(i, j));
+        }
+      }
+      EXPECT_EQ(block.shared_memory_used(), A.shared_memory_used());
+      n_start += block.cols;
+    }
+    m_start += split[i_c * (col_split_indices.size()+1)].rows;
   }
 }
