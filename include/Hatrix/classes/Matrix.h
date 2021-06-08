2952d0417fbc1e7131199a4dafa5b26d1c097afc
derpda <peter.spalthoff@gmail.com>
2021-06-08 10:46:54 +0900

    split functions with tests added

--- include/Hatrix/classes/Matrix.h
+++ include/Hatrix/classes/Matrix.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <cstdint>
+#include <cstddef>
 #include <memory>
+#include <vector>
 
 namespace Hatrix {
 
@@ -40,10 +42,21 @@ class Matrix {
 
   void shrink(int64_t rows, int64_t cols);
 
+  std::vector<Matrix> split(int64_t n_row_splits, int64_t n_col_splits,
+                            bool copy = false) const;
+
+  std::vector<Matrix> split(const std::vector<int64_t>& row_split_indices,
+                            const std::vector<int64_t>& col_split_indices,
+                            bool copy = false) const;
+
   int64_t min_dim() const;
   int64_t max_dim() const;
 
   void print() const;
+
+  size_t memory_used() const;
+
+  size_t shared_memory_used() const;
 };
 
 }  // namespace Hatrix
