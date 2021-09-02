482930dae5cda2a230fc87807cf161a1a13e5e9c
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2021-09-02 16:12:35 +0900

    start running tests for blocked structure

--- run.sh
+++ run.sh
@@ -2,22 +2,17 @@
 #YBATCH -r dgx-a100_2
 #SBATCH -N 1
 #SBATCH -J UMV
-#SBATCH --time=12:00:00
+#SBATCH --time=160:00:00
 
 make clean
 make -j all
 
-FILE=test_hatrix.csv
+FILE=blr2_epyc_copy_blocks.csv
 
-rm $FILE
-./bin/UMV_weak_1level 1000 10 100 $FILE
-./bin/UMV_weak_1level 1000 100 100 $FILE
-cat $FILE
-
-# for N in 1000 5000 10000 20000 40000 80000; do
-#     for rank in 5 10 20 40 60 100; do
-#         for block in 100 500 1000 2000 4000; do
-#             ./bin/UMV_weak_1level $N $rank $block $FILE
-#         done
-#     done
-# done
+for N in 1000 5000 10000 20000 40000 80000; do
+    for rank in 5 10 20 40 60 100; do
+        for block in 100 500 1000 2000 4000; do
+            ./bin/UMV_weak_1level $N $rank $block $FILE
+        done
+    done
+done
