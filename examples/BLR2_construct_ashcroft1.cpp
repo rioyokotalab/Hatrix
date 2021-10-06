9a72b72be2cab3450b037ad9335cc6230092ce54
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2021-10-06 12:38:51 +0900

    adding ashcroft construction

---
+++ examples/BLR2_construct_ashcroft1.cpp
@@ -0,0 +1,106 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+#include <tuple>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "Hatrix/Hatrix.h"
+
+using randvec_t = std::vector<std::vector<double> >;
+
+std::vector<double> equally_spaced_vector(int N, double minVal, double maxVal) {
+  std::vector<double> res(N, 0.0);
+  double rnge = maxVal - minVal;
+  for(int i=0; i<N; i++) {
+    res[i] = minVal + ((double)i/(double)rnge);
+  }
+  return res;
+}
+
+double rel_error(const Hatrix::Matrix& A, const Hatrix::Matrix& B) {
+  return Hatrix::norm(A - B) / Hatrix::norm(B);
+}
+
+namespace Hatrix {
+  class BLR2 {
+  private:
+    RowMap U;
+    ColMap V;
+    RowColMap<bool> is_admissible;
+    RowColMap<Matrix> D, S;
+    int64_t N, nblocks, rank, admis;
+
+  public:
+    BLR2(const randvec_t& randpts, int64_t N, int64_t nblocks, int64_t rank, int64_t admis) :
+      N(N), nblocks(nblocks), rank(rank), admis(admis) {
+      int block_size = N / nblocks;
+
+      for (int i = 0; i < nblocks; ++i) {
+        for (int j = 0; j < nblocks; ++j) {
+          is_admissible.insert(i, j, std::abs(i - j) > admis);
+
+          if (!is_admissible(i, j)) {
+            D.insert(i, j,
+                     Hatrix::generate_laplacend_matrix(randpts,
+                                                       block_size, block_size,
+                                                       i*block_size, j*block_size));
+          }
+        }
+      }
+    }
+
+    double construction_error(const randvec_t& randpts) {
+      double error = 0;
+      int block_size = N / nblocks;
+
+      for (int i = 0; i < nblocks; ++i) {
+        for (int j = 0; j < nblocks; ++j) {
+          if (!is_admissible(i, j)) {
+            double dense_error = Hatrix::norm(D(i, j) -
+                                           Hatrix::generate_laplacend_matrix(randpts, block_size, block_size,
+                                                                             block_size * i, block_size * j));
+            error += dense_error;
+          }
+          else {
+            Matrix& Ubig = U(i);
+            Matrix& Vbig = V(j);
+            Matrix expected = matmul(matmul(Ubig, S(i, j)), Vbig, false, true);
+            Matrix actual = Hatrix::generate_laplacend_matrix(randpts, block_size, block_size,
+                                                              i * block_size, j * block_size);
+            error += Hatrix::norm(expected - actual);
+          }
+        }
+      }
+      return std::sqrt(error / N / N);
+    }
+  };
+}
+
+int main(int argc, char** argv) {
+  int64_t N = atoi(argv[1]);
+  int64_t nblocks = atoi(argv[2]);
+  int64_t rank = atoi(argv[3]);
+  int64_t admis = atoi(argv[4]);
+
+  Hatrix::Context::init();
+  randvec_t randpts;
+  randpts.push_back(equally_spaced_vector(N, 0.0, 1.0 * N)); // 1D
+  randpts.push_back(equally_spaced_vector(N, 0.0, 1.0 * N)); // 2D
+  randpts.push_back(equally_spaced_vector(N, 0.0, 1.0 * N)); // 3D
+
+  if (N % nblocks != 0) {
+    std::cout << "N % nblocks != 0. Aborting.\n";
+    abort();
+  }
+
+  Hatrix::BLR2 A(randpts, N, nblocks, rank, admis);
+  double construct_error = A.construction_error(randpts);
+
+  Hatrix::Context::finalize();
+
+  std::cout << "N: " << N << " rank: " << rank << " nblocks: " << nblocks << " admis: " <<  admis
+            << " construct error: " << construct_error << "\n";
+}
