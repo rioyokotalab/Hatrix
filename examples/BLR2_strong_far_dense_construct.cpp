da9afc835826ae883a1be6c2b35384d7ae335cf1
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2021-10-21 14:43:35 +0900

    low rank generation

--- examples/BLR2_strong_far_dense_construct.cpp
+++ examples/BLR2_strong_far_dense_construct.cpp
@@ -71,7 +71,13 @@ namespace Hatrix {
     // Map of inadmissible indices.
     RowColMap<bool> is_admissible;
     // Vector of vector for storing the actual indices of all the inadmissible blocks in a given row.
-    std::vector<std::vector<int64_t> > inadmissible_indices;
+    std::vector<std::vector<int64_t> > inadmissible_row_indices, admissible_row_indices;
+    std::vector<std::vector<int64_t> > inadmissible_col_indices, admissible_col_indices;
+
+    RowMap U;
+    ColMap V;
+    RowColMap<Matrix> S;
+
     int64_t N, nblocks, rank, ndim;
     double admis;
 
@@ -92,10 +98,15 @@ namespace Hatrix {
 
   public:
     BLR2(const std::vector<Hatrix::Particle>& particles, int64_t N, int64_t nblocks,
-         int64_t rank, double admis, int64_t ndim) :
-      N(N), nblocks(nblocks), rank(rank), admis(admis), ndim(ndim) {
+         int64_t rank, int64_t ndim, double admis) :
+      N(N), nblocks(nblocks), rank(rank), ndim(ndim), admis(admis) {
+      inadmissible_row_indices.resize(nblocks);
+      admissible_row_indices.resize(nblocks);
+
+      inadmissible_col_indices.resize(nblocks);
+      admissible_col_indices.resize(nblocks);
+
       int64_t block_size = N / nblocks;
-      inadmissible_indices.resize(nblocks);
       auto boxes = create_particle_boxes(particles);
 
       for (int i = 0; i < nblocks; ++i) {
@@ -104,11 +115,81 @@ namespace Hatrix {
                                admis * boxes[i].distance_from(boxes[j]));
 
           if (!is_admissible(i, j)) {
-            inadmissible_indices[i].push_back(j);
             D.insert({i, generate_laplacend_matrix(particles,
                                                    block_size, block_size,
                                                    i*block_size, j*block_size, ndim)});
           }
+        }
+      }
+
+      for (int i = 0; i < nblocks; ++i) {
+        for (int j = 0; j < nblocks; ++j) {
+          if (is_admissible(i, j)) {
+            admissible_row_indices[i].push_back(j);
+          }
+          else {
+            inadmissible_row_indices[i].push_back(j);
+          }
+        }
+      }
+
+      for (int j = 0; j < nblocks; ++j) {
+        for (int i = 0; i < nblocks; ++i) {
+          if (is_admissible(j, i)) {
+            admissible_col_indices[j].push_back(i);
+          }
+          else {
+            inadmissible_col_indices[j].push_back(i);
+          }
+        }
+      }
+
+      int64_t oversampling = 5;
+      Hatrix::Matrix Utemp, Stemp, Vtemp;
+      double error;
+      std::vector<Hatrix::Matrix> Y;
+
+      // Generate a bunch of random matrices.
+      for (int64_t i = 0; i < nblocks; ++i) {
+        Y.push_back(
+                    Hatrix::generate_random_matrix(block_size, rank + oversampling));
+      }
+
+      for (int64_t i = 0; i < nblocks; ++i) {
+        Hatrix::Matrix AY(block_size, rank + oversampling);
+        for (unsigned j = 0; j < admissible_row_indices[i].size(); ++j) {
+          int64_t jcol = admissible_row_indices[i][j];
+          Hatrix::Matrix dense = Hatrix::generate_laplacend_matrix(particles,
+                                                                   block_size, block_size,
+                                                                   i*block_size, jcol*block_size, ndim);
+          Hatrix::matmul(dense, Y[jcol], AY);
+        }
+        std::tie(Utemp, Stemp, Vtemp, error) = Hatrix::truncated_svd(AY, rank);
+        U.insert(i, std::move(Utemp));
+      }
+
+      for (int64_t j = 0; j < nblocks; ++j) {
+        Hatrix::Matrix YtA(rank + oversampling, block_size);
+
+        for (long unsigned int i = 0; i < admissible_col_indices[j].size(); ++i) {
+          int64_t irow = admissible_col_indices[j][i];
+          Hatrix::Matrix dense = Hatrix::generate_laplacend_matrix(particles,
+                                                                   block_size, block_size,
+                                                                   irow*block_size, j*block_size, ndim);
+          Hatrix::matmul(Y[irow], dense, YtA, true);
+        }
+        std::tie(Utemp, Stemp, Vtemp, error) = Hatrix::truncated_svd(YtA, rank);
+        V.insert(j, std::move(transpose(Vtemp)));
+      }
+
+      for (int i = 0; i < nblocks; ++i) {
+        for (unsigned j = 0; j < admissible_row_indices[j].size(); ++j) {
+          int64_t jcol = admissible_row_indices[i][j];
+          Hatrix::Matrix dense = Hatrix::generate_laplacend_matrix(particles,
+                                                                   block_size, block_size,
+                                                                   i*block_size, jcol*block_size, ndim);
+          S.insert(i, jcol,
+                   Hatrix::matmul(Hatrix::matmul(U[i], dense, true), V[jcol]));
         }
       }
     }
@@ -124,7 +205,7 @@ namespace Hatrix {
 
         int j = 0;
         for (std::multimap<int64_t, Matrix>::iterator it = row_dense_blocks.first; it != row_dense_blocks.second; ++it) {
-          int64_t icol = inadmissible_indices[i][j];
+          int64_t icol = inadmissible_row_indices[i][j];
           auto dense = generate_laplacend_matrix(particles,
                                                  block_size, block_size,
                                                  i*block_size, icol*block_size, ndim);
@@ -175,7 +256,7 @@ int main(int argc, char** argv) {
     abort();
   }
 
-  Hatrix::BLR2 A(particles, N, nblocks, rank, admis, ndim);
+  Hatrix::BLR2 A(particles, N, nblocks, rank, ndim, admis);
   A.print_structure();
   double construct_error = A.construction_error(particles);
 
