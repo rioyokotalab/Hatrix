ddda26baee5b4c8f7f4c41dcd85aa4c6eb88124b
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2021-10-24 22:34:26 +0900

    add a new Domain class for particle subdivisions

--- examples/BLR2_strong_far_dense_construct.cpp
+++ examples/BLR2_strong_far_dense_construct.cpp
@@ -59,6 +59,86 @@ namespace Hatrix {
     }
   };
 
+  class Domain {
+  public:
+    std::vector<Hatrix::Particle> particles;
+    std::vector<Hatrix::Box> boxes;
+    int64_t N, ndim;
+
+  private:
+    void orthogonal_recursive_bisection_1dim(const std::vector<Hatrix::Particle>& particles, int64_t start,
+                                             int64_t end, std::vector<double>& sorted_x,
+                                             std::string morton_index, std::vector<Box>& boxes, int64_t nleaf) {
+      int64_t num_points = end - start;
+      // found a box with the correct number of points.
+      if (num_points <= nleaf) {
+        auto start_x = particles[start].coords[0];
+        auto stop_x = particles[end-1].coords[0];
+        auto center_x = (start_x + stop_x) / 2;
+        auto diameter = stop_x - start_x;
+        boxes.push_back(Box(diameter, center_x, start_x, stop_x, morton_index, num_points));
+      }
+      else {                    // recurse further and split again.
+        int64_t middle = (start + end) / 2;
+        // first half
+        orthogonal_recursive_bisection_1dim(particles, start, middle, sorted_x, morton_index + "0", boxes, nleaf);
+        // second half
+        orthogonal_recursive_bisection_1dim(particles, middle, end, sorted_x, morton_index + "1", boxes, nleaf);
+      }
+    }
+  public:
+
+    Domain(int64_t N, int64_t ndim) : N(N), ndim(ndim) {}
+
+    void equally_spaced_particles(double min_val, double max_val) {
+      double range = max_val - min_val;
+
+      if (ndim == 1) {
+        for (int64_t i = 0; i < N; ++i) {
+          particles.push_back(Hatrix::Particle(i*0.4, min_val + (double(i) / double(range))));
+        }
+      }
+      else if (ndim == 3) {
+        // Generate a unit sphere geometry with N points on the surface.
+        // http://www.cpp.re/forum/windows/262648/
+        // https://neil-strickland.staff.shef.ac.uk/courses/MAS243/lectures/handout10.pdf
+        std::random_device rd;  // Will be used to obtain a seed for the random number engine
+        std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
+        std::uniform_real_distribution<> dis(0.0, 2.0 * M_PI);
+        double radius = 1.0;
+        for (int64_t i = 0; i < N; ++i) {
+          double phi = dis(gen);
+          double theta = dis(gen);
+
+          double x = radius * sin(phi) * cos(theta);
+          double y = radius * sin(phi) * sin(theta);
+          double z = radius * cos(phi);
+
+          particles.push_back(Hatrix::Particle(x, y, z, min_val + (double(i) / double(range))));
+        }
+      }
+    }
+
+    void divide_domain_and_create_particle_boxes(int64_t nleaf) {
+      if (ndim == 1) {
+        // Keep a sorted
+        std::vector<double> sorted_x(N);
+        for (int i = 0; i < N; ++i) { sorted_x[i] = particles[i].coords[0]; }
+        std::sort(sorted_x.begin(), sorted_x.end());
+
+        int64_t start = 0;
+        int64_t end = N;
+        std::string morton_index = "";
+
+        orthogonal_recursive_bisection_1dim(particles, start, end, sorted_x, morton_index, boxes, nleaf);
+      }
+      else if (ndim == 3) {
+
+      }
+    }
+  };
+
+
   // Generate a full dense laplacian matrix assuming unit charges.
   Matrix generate_laplacend_matrix(const std::vector<Hatrix::Particle>& particles,
                                    int64_t nrows, int64_t ncols, int64_t ndim) {
@@ -89,50 +169,6 @@ namespace Hatrix {
     return out;
   }
 
-  void orthogonal_recursive_bisection_1dim(const std::vector<Hatrix::Particle>& particles, int64_t start,
-                                           int64_t end, std::vector<double>& sorted_x,
-                                           std::string morton_index, std::vector<Box>& boxes, int64_t nleaf) {
-    int num_points = end - start;
-    // found a box with the correct number of points.
-    if (num_points <= nleaf) {
-      auto start_x = particles[start].coords[0];
-      auto stop_x = particles[end-1].coords[0];
-      auto center_x = (start_x + stop_x) / 2;
-      auto diameter = stop_x - start_x;
-      boxes.push_back(Box(diameter, center_x, start_x, stop_x, morton_index, num_points));
-    }
-    else {                    // recurse further and split again.
-      int64_t middle = (start + end) / 2;
-      // first half
-      orthogonal_recursive_bisection_1dim(particles, start, middle, sorted_x, morton_index + "0", boxes, nleaf);
-      // second half
-      orthogonal_recursive_bisection_1dim(particles, middle, end, sorted_x, morton_index + "1", boxes, nleaf);
-    }
-  }
-
-  std::vector<Box> divide_domain_and_create_particle_boxes(const std::vector<Hatrix::Particle>& particles,
-                                                           int64_t N, int64_t ndim, int64_t nleaf) {
-    std::vector<Box> boxes;
-
-    if (ndim == 1) {
-      // Keep a sorted
-      std::vector<double> sorted_x(N);
-      for (int i = 0; i < N; ++i) { sorted_x[i] = particles[i].coords[0]; }
-      std::sort(sorted_x.begin(), sorted_x.end());
-
-      int64_t start = 0;
-      int64_t end = N;
-      std::string morton_index = "";
-
-      orthogonal_recursive_bisection_1dim(particles, start, end, sorted_x, morton_index, boxes, nleaf);
-    }
-    else if (ndim == 3) {
-
-    }
-
-    return boxes;
-  }
-
   class BLR2 {
   private:
     // Store the dense blocks in a multimap for faster iteration without hash lookups.
@@ -151,10 +187,10 @@ namespace Hatrix {
     double admis;
 
   public:
-    BLR2(const std::vector<Hatrix::Particle>& particles, const std::vector<Hatrix::Box>& boxes, int64_t N,
+    BLR2(const Domain& domain, int64_t N,
          int64_t nleaf, int64_t rank, int64_t ndim, double admis) :
       N(N), nleaf(nleaf), rank(rank), ndim(ndim), admis(admis) {
-      nblocks = boxes.size();
+      nblocks = domain.boxes.size();
 
       inadmissible_row_indices.resize(nblocks);
       admissible_row_indices.resize(nblocks);
@@ -164,11 +200,13 @@ namespace Hatrix {
 
       for (int i = 0; i < nblocks; ++i) {
         for (int j = 0; j < nblocks; ++j) {
-          is_admissible.insert(i, j, std::min(boxes[i].diameter, boxes[j].diameter) <=
-                               admis * boxes[i].distance_from(boxes[j]));
+          is_admissible.insert(i, j, std::min(domain.boxes[i].diameter, domain.boxes[j].diameter) <=
+                               admis * domain.boxes[i].distance_from(domain.boxes[j]));
 
           if (!is_admissible(i, j)) {
-            D.insert({i, generate_p2p_interactions(boxes, i, j, ndim)});
+            D.insert({i, generate_p2p_interactions(domain.boxes, i, j, ndim)});
+
+            // generate_p2p_interactions(domain.boxes, i, j, ndim).print();
           }
         }
       }
@@ -200,30 +238,26 @@ namespace Hatrix {
       double error;
       std::vector<Hatrix::Matrix> Y;
 
-      // Generate a bunch of random matrices.
       for (int64_t i = 0; i < nblocks; ++i) {
-        Y.push_back(
-                    Hatrix::generate_random_matrix(nleaf, rank + oversampling));
-      }
-
-      for (int64_t i = 0; i < nblocks; ++i) {
-        Hatrix::Matrix AY(nleaf, rank + oversampling);
+        Hatrix::Matrix AY(domain.boxes[i].num_particles, rank + oversampling);
         for (unsigned j = 0; j < admissible_row_indices[i].size(); ++j) {
           int64_t jcol = admissible_row_indices[i][j];
-          Hatrix::Matrix dense = generate_p2p_interactions(boxes, i, jcol, ndim);
-          Hatrix::matmul(dense, Y[jcol], AY);
+          Hatrix::Matrix dense = generate_p2p_interactions(domain.boxes, i, jcol, ndim);
+          Hatrix::Matrix random_matrix = generate_random_matrix(dense.cols, rank + oversampling);
+          Hatrix::matmul(dense, random_matrix, AY);
         }
         std::tie(Utemp, Stemp, Vtemp, error) = Hatrix::truncated_svd(AY, rank);
         U.insert(i, std::move(Utemp));
       }
 
       for (int64_t j = 0; j < nblocks; ++j) {
-        Hatrix::Matrix YtA(rank + oversampling, nleaf);
+        Hatrix::Matrix YtA(rank + oversampling, domain.boxes[j].num_particles);
 
         for (long unsigned int i = 0; i < admissible_col_indices[j].size(); ++i) {
           int64_t irow = admissible_col_indices[j][i];
-          Hatrix::Matrix dense = Hatrix::generate_p2p_interactions(boxes, irow, j, ndim);
-          Hatrix::matmul(Y[irow], dense, YtA, true);
+          Hatrix::Matrix dense = Hatrix::generate_p2p_interactions(domain.boxes, irow, j, ndim);
+          Hatrix::Matrix random_matrix = generate_random_matrix(dense.rows, rank + oversampling);
+          Hatrix::matmul(random_matrix, dense, YtA, true);
         }
         std::tie(Utemp, Stemp, Vtemp, error) = Hatrix::truncated_svd(YtA, rank);
         V.insert(j, std::move(transpose(Vtemp)));
@@ -232,13 +266,13 @@ namespace Hatrix {
       for (int i = 0; i < nblocks; ++i) {
         for (unsigned j = 0; j < admissible_row_indices[i].size(); ++j) {
           int64_t jcol = admissible_row_indices[i][j];
-          Hatrix::Matrix dense = Hatrix::generate_p2p_interactions(boxes, i, jcol, ndim);
+          Hatrix::Matrix dense = Hatrix::generate_p2p_interactions(domain.boxes, i, jcol, ndim);
           S.insert(i, jcol, Hatrix::matmul(Hatrix::matmul(U[i], dense, true), V[jcol]));
         }
       }
     }
 
-    double construction_error(const std::vector<Hatrix::Particle>& particles, const std::vector<Hatrix::Box>& boxes) {
+    double construction_error(const Domain& domain) {
       // Check dense blocks
       double error = 0; double dense_norm = 0;
 
@@ -249,7 +283,7 @@ namespace Hatrix {
         int j = 0;
         for (std::multimap<int64_t, Matrix>::iterator it = row_dense_blocks.first; it != row_dense_blocks.second; ++it) {
           int64_t jcol = inadmissible_row_indices[i][j];
-          auto dense = Hatrix::generate_p2p_interactions(boxes, i, jcol, ndim);
+          auto dense = Hatrix::generate_p2p_interactions(domain.boxes, i, jcol, ndim);
           dense_norm += pow(norm(dense), 2);
           error += pow(norm(it->second -  dense), 2);
           j++;
@@ -259,11 +293,11 @@ namespace Hatrix {
       for (unsigned i = 0; i < nblocks; ++i) {
         for (unsigned j = 0; j < admissible_row_indices[i].size(); ++j) {
           int64_t jcol = admissible_row_indices[i][j];
-          auto dense = generate_p2p_interactions(boxes, i, jcol, ndim);
+          auto dense = generate_p2p_interactions(domain.boxes, i, jcol, ndim);
           Matrix& Ubig = U(i);
           Matrix& Vbig = V(jcol);
           Matrix expected = matmul(matmul(Ubig, S(i, jcol)), Vbig, false, true);
-          Matrix actual = generate_p2p_interactions(boxes, i, jcol, ndim);
+          Matrix actual = generate_p2p_interactions(domain.boxes, i, jcol, ndim);
           error += pow(norm(expected - actual), 2);
           dense_norm += pow(norm(actual), 2);
         }
@@ -273,6 +307,7 @@ namespace Hatrix {
     }
 
     void print_structure() {
+      std::cout << "BLR " << nblocks << " x " << nblocks << std::endl;
       for (int i = 0; i < nblocks; ++i) {
         for (int j = 0; j < nblocks; ++j) {
           std::cout << " | " << is_admissible(i, j);
@@ -283,39 +318,6 @@ namespace Hatrix {
   };
 }
 
-std::vector<Hatrix::Particle> equally_spaced_particles(int64_t ndim, int64_t N,
-                                                       double min_val, double max_val) {
-  std::vector<Hatrix::Particle> particles;
-  double range = max_val - min_val;
-
-  if (ndim == 1) {
-    for (int64_t i = 0; i < N; ++i) {
-      particles.push_back(Hatrix::Particle(i*0.4, min_val + (double(i) / double(range))));
-    }
-  }
-  else if (ndim == 3) {
-    // Generate a unit sphere geometry with N points on the surface.
-    // http://www.cpp.re/forum/windows/262648/
-    // https://neil-strickland.staff.shef.ac.uk/courses/MAS243/lectures/handout10.pdf
-    std::random_device rd;  // Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
-    std::uniform_real_distribution<> dis(0.0, 2.0 * M_PI);
-    double radius = 1.0;
-    for (int64_t i = 0; i < N; ++i) {
-      double phi = dis(gen);
-      double theta = dis(gen);
-
-      double x = radius * sin(phi) * cos(theta);
-      double y = radius * sin(phi) * sin(theta);
-      double z = radius * cos(phi);
-
-      particles.push_back(Hatrix::Particle(x, y, z, min_val + (double(i) / double(range))));
-    }
-  }
-
-
-  return particles;
-}
 
 int main(int argc, char** argv) {
   int64_t N = atoi(argv[1]);
@@ -325,19 +327,20 @@ int main(int argc, char** argv) {
   int64_t ndim = atoi(argv[5]);
 
   Hatrix::Context::init();
-  auto particles = equally_spaced_particles(ndim, N, 0.0, 1.0 * N);
-  auto boxes = divide_domain_and_create_particle_boxes(particles, N, ndim, nleaf);
+  Hatrix::Domain domain(N, ndim);
+  domain.equally_spaced_particles(0.0, 1.0 * N);
+  domain.divide_domain_and_create_particle_boxes(nleaf);
 
   if (N % nleaf != 0) {
     std::cout << "N % nleaf != 0. Aborting.\n";
     abort();
   }
 
-  Hatrix::BLR2 A(particles, boxes, N, nleaf, rank, ndim, admis);
+  Hatrix::BLR2 A(domain, N, nleaf, rank, ndim, admis);
   A.print_structure();
-  double construct_error = A.construction_error(particles, boxes);
+  double construct_error = A.construction_error(domain);
 
-  Hatrix::Matrix dense = Hatrix::generate_laplacend_matrix(particles, N, N, 1);
+  Hatrix::Matrix dense = Hatrix::generate_laplacend_matrix(domain.particles, N, N, 1);
 
   Hatrix::Context::finalize();
 
