00d1a18cf5926f0ba518048f1df1c0637cc2db36
wawando <muhammad.ridwan.ab@gmail.com>
2021-10-25 11:59:19 +0900

    LDL weak N level finished

--- examples/LDL_weak_Nlevel.cpp
+++ examples/LDL_weak_Nlevel.cpp
@@ -14,21 +14,19 @@ using vec = std::vector<int64_t>;
 using randvec_t = std::vector<std::vector<double> >;
 
 namespace Hatrix {
-  class HSS {
+  class HSS_SPD {
   public:
     RowLevelMap U, Uc;
-    ColLevelMap V;
     RowColLevelMap D, S;
     int64_t N, rank, leaf_size, height;
 
-    HSS(const randvec_t& randpts, int64_t N, int64_t rank, int64_t height):
+    HSS_SPD(const randvec_t& randpts, int64_t N, int64_t rank, int64_t height):
       N(N), rank(rank), height(height), leaf_size(N/pow(2, height))
     {
       generate_leaf_nodes(randpts);
       RowLevelMap Uchild = U;
-      ColLevelMap Vchild = V;
       for(int64_t level = height-1; level > 0; level--) {
-	std::tie(Uchild, Vchild) = generate_non_leaf_nodes(randpts, level, Uchild, Vchild);
+	Uchild = generate_non_leaf_nodes(randpts, level, Uchild);
       }
     }
 
@@ -47,23 +45,6 @@ namespace Hatrix {
       matmul(Ubig_child1, transfer_matrix_splits[0], U_big_splits[0]);
       matmul(Ubig_child2, transfer_matrix_splits[1], U_big_splits[1]);
       return U_big;
-    }
-
-    Matrix Vbig(int64_t col, int64_t level) {
-      if(level == height) return V(col, level);
-
-      int64_t child1 = 2*col;
-      Matrix Vbig_child1 = Vbig(child1, level+1);
-      int64_t child2 = 2*col + 1;
-      Matrix Vbig_child2 = Vbig(child2, level+1);
-
-      int64_t node_size = Vbig_child1.rows + Vbig_child2.rows;
-      Matrix V_big(node_size, rank);
-      auto V_big_splits = V_big.split(vec{Vbig_child1.rows}, vec());
-      auto transfer_matrix_splits = V(col, level).split(2, 1);
-      matmul(Vbig_child1, transfer_matrix_splits[0], V_big_splits[0]);
-      matmul(Vbig_child2, transfer_matrix_splits[1], V_big_splits[1]);
-      return V_big;
     }
 
     Hatrix::Matrix Uf(int64_t row, int64_t level) {
@@ -98,8 +79,8 @@ namespace Hatrix {
 	  fnorm = Hatrix::norm(Dij);
 	  norm += fnorm * fnorm;
 	  Matrix Urow = Ubig(row, level);
-	  Matrix Vcol = Vbig(col, level);
-	  Matrix Aij = matmul(matmul(Urow, S(row, col, level)), Vcol, false, true);
+	  Matrix Ucol = Ubig(col, level);
+	  Matrix Aij = matmul(matmul(Urow, S(row, col, level)), Ucol, false, true);
 	  fdiff = Hatrix::norm(Aij - Dij);
 	  diff += fdiff * fdiff;
 	}
@@ -125,35 +106,11 @@ namespace Hatrix {
       return row_slice;
     }
 
-    Matrix admissible_column_slice(int64_t col, int64_t level,
-				   const randvec_t& randpts) {
-      int64_t num_nodes = pow(2, level);
-      int64_t node_size = N / num_nodes;	
-      Matrix col_slice((num_nodes - 1) * node_size, node_size);
-      auto col_slice_splits = col_slice.split(num_nodes - 1, 1);
-      
-      int64_t i = 0;
-      for(int64_t row = 0; row < num_nodes; row++) {
-	if(row == col) continue;
-	Matrix block = generate_laplacend_matrix(randpts, node_size, node_size,
-						 row*node_size, col*node_size);
-	col_slice_splits[i++] = block;
-      }
-      return col_slice;
-    }
-
     Matrix generate_leaf_column_bases(int64_t row, const randvec_t& randpts) {
       Matrix row_slice = admissible_row_slice(row, height, randpts);
       Matrix U;
       std::tie(U, std::ignore, std::ignore, std::ignore) = truncated_svd(row_slice, rank);
       return U;
-    }
-
-    Matrix generate_leaf_row_bases(int64_t col, const randvec_t& randpts) {
-      Matrix col_slice = admissible_column_slice(col, height, randpts);
-      Matrix V;
-      std::tie(std::ignore, std::ignore, V, std::ignore) = truncated_svd(col_slice, rank);
-      return transpose(V);
     }
 
     void generate_leaf_nodes(const randvec_t& randpts) {
@@ -164,12 +121,10 @@ namespace Hatrix {
 		 generate_laplacend_matrix(randpts, leaf_size, leaf_size,
 					   node*leaf_size, node*leaf_size));
       }
-      //Generate U and V bases
+      //Generate U bases
       for(int64_t node = 0; node < num_nodes; node++) {
 	U.insert(node, height,
 		 generate_leaf_column_bases(node, randpts));
-	V.insert(node, height,
-		 generate_leaf_row_bases(node, randpts));
       }
       //Generate coupling matrices
       for(int64_t row = 0; row < num_nodes; row++) {
@@ -177,14 +132,12 @@ namespace Hatrix {
 	Matrix D = generate_laplacend_matrix(randpts, leaf_size, leaf_size,
 					     row*leaf_size, col*leaf_size);
 	S.insert(row, col, height,
-		 matmul(matmul(U(row, height), D, true), V(col, height)));
+		 matmul(matmul(U(row, height), D, true), U(col, height)));
       }
     }
 
-    std::tuple<RowLevelMap, ColLevelMap> generate_non_leaf_nodes(const randvec_t& randpts,
-								 int64_t level,
-								 RowLevelMap& Ubig_child,
-								 ColLevelMap& Vbig_child) {
+    RowLevelMap generate_non_leaf_nodes(const randvec_t& randpts, int64_t level,
+					RowLevelMap& Ubig_child) {
       int64_t num_nodes = pow(2, level);
       int64_t node_size = N / num_nodes;
       int64_t child_level = level + 1;
@@ -193,7 +146,6 @@ namespace Hatrix {
       //Save the generated bases of the current level and pass them to this
       //function again for generating upper level nodes
       RowLevelMap Ubig_node;
-      ColLevelMap Vbig_node;
       
       //Generate transfer matrices
       for(int64_t node = 0; node < num_nodes; node++) {
@@ -217,23 +169,7 @@ namespace Hatrix {
 	std::tie(Utransfer, std::ignore, std::ignore, std::ignore) =
 	  truncated_svd(projected_row_slice, rank);
 	U.insert(node, level, std::move(Utransfer));
-	
-	//V transfer matrix
-	Matrix& Vbig_child1 = Vbig_child(child1, child_level);
-	Matrix& Vbig_child2 = Vbig_child(child2, child_level);
-	Matrix col_slice = admissible_column_slice(node, level, randpts);
-	auto col_slice_splits = col_slice.split(1, 2);
-	Matrix projected_col_slice(col_slice.rows,
-				   Vbig_child1.cols + Vbig_child2.cols);
-	auto projected_col_slice_splits = projected_col_slice.split(1, 2);
-	matmul(col_slice_splits[0], Vbig_child1, projected_col_slice_splits[0]);
-	matmul(col_slice_splits[1], Vbig_child2, projected_col_slice_splits[1]);
-	
-	Matrix Vtransfer;
-	std::tie(std::ignore, std::ignore, Vtransfer, std::ignore) =
-	  truncated_svd(projected_col_slice, rank);
-	V.insert(node, level, transpose(Vtransfer));
-	
+		
 	//Generate bases to passed for generating upper level transfer matrices
 	auto Utransfer_splits = U(node, level).split(2, 1);
 	Matrix U_big(node_size, rank);
@@ -241,13 +177,6 @@ namespace Hatrix {
 	matmul(Ubig_child1, Utransfer_splits[0], U_big_splits[0]);
 	matmul(Ubig_child2, Utransfer_splits[1], U_big_splits[1]);
 	Ubig_node.insert(node, level, std::move(U_big));
-	
-	auto Vtransfer_splits = V(node, level).split(2, 1);
-	Matrix V_big(node_size, rank);
-	auto V_big_splits = V_big.split(2, 1);
-	matmul(Vbig_child1, Vtransfer_splits[0], V_big_splits[0]);
-	matmul(Vbig_child2, Vtransfer_splits[1], V_big_splits[1]);
-	Vbig_node.insert(node, level, std::move(V_big));
       }
       
       //Generate coupling matrices
@@ -255,12 +184,11 @@ namespace Hatrix {
 	int64_t col = row % 2 == 0 ? row + 1 : row - 1;
 	Matrix D = generate_laplacend_matrix(randpts, node_size, node_size,
 					     row*node_size, col*node_size);
-	S.insert(row, col, level,
-		 matmul(matmul(Ubig_node(row, level), D, true),
-			Vbig_node(col, level)));
+	S.insert(row, col, level, matmul(matmul(Ubig_node(row, level), D, true),
+					 Ubig_node(col, level)));
       }
 
-      return {Ubig_node, Vbig_node};
+      return Ubig_node;
     }
   };
 }
@@ -289,8 +217,8 @@ Hatrix::Matrix make_complement(const Hatrix::Matrix& _U) {
   return Uc;
 }
 
-Hatrix::Matrix& get_oo_part(Hatrix::HSS& A,
-			   int64_t row, int64_t col, int64_t level) {
+Hatrix::Matrix& get_oo_part(Hatrix::HSS_SPD& A,
+			    int64_t row, int64_t col, int64_t level) {
   if(row == col) { //Dense diagonal
     int64_t c_size = A.D(row, col, level).rows - A.rank;
     auto D_splits = A.D(row, col, level).split(vec{c_size}, vec{c_size});
@@ -301,7 +229,7 @@ Hatrix::Matrix& get_oo_part(Hatrix::HSS& A,
   }
 }
 
-void partial_ldl_diag(Hatrix::HSS& A, int64_t node, int64_t level) {
+void partial_ldl_diag(Hatrix::HSS_SPD& A, int64_t node, int64_t level) {
   int64_t c_size = A.D(node, node, level).rows - A.rank;
   auto D_splits = A.D(node, node, level).split(vec{c_size}, vec{c_size});
   Hatrix::ldl(D_splits[0]);
@@ -315,7 +243,7 @@ void partial_ldl_diag(Hatrix::HSS& A, int64_t node, int64_t level) {
   Hatrix::matmul(L_oc, D_splits[2], D_splits[3], false, true, -1., 1.);
 }
 
-void factorize(Hatrix::HSS& A) {
+void factorize(Hatrix::HSS_SPD& A) {
   //Bottom up from leaf to root
   for(int64_t level = A.height; level > 0; level--) {
     int64_t num_nodes = pow(2, level);
@@ -345,10 +273,164 @@ void factorize(Hatrix::HSS& A) {
   Hatrix::ldl(A.D(0, 0, 0));
 }
 
+void solve(Hatrix::HSS_SPD& A, Hatrix::Matrix& b) {
+  //Split b according to leaf level partitioning
+  vec leaf_co_split_indices;
+  int64_t num_leaf_nodes = pow(2, A.height);
+  for(int node = 0; node < num_leaf_nodes; node++) {
+    Hatrix::Matrix& D = A.D(node, node, A.height);
+    int64_t c_size = D.rows - A.rank;
+    leaf_co_split_indices.push_back(node*A.leaf_size + c_size);
+    if(node < (num_leaf_nodes - 1)) {
+      leaf_co_split_indices.push_back(node*A.leaf_size + c_size + A.rank);
+    }
+  }
+  auto b_leaf_co_splits = b.split(leaf_co_split_indices, vec());
+  
+  //---Forward Substitution---
+  for(int64_t level = A.height; level > 0; level--) {
+    int64_t num_nodes = pow(2, level);
+    int64_t splits_per_node = pow(2, A.height - level + 1);
+    for(int64_t node = 0; node < num_nodes; node++) {
+      //Get c and o slices of the vector to be operated
+      int64_t o_index = splits_per_node * (node + 1) - 1;
+      int64_t c_index = o_index - (splits_per_node/2);
+      Hatrix::Matrix& b_node_c = b_leaf_co_splits[c_index];
+      Hatrix::Matrix& b_node_o = b_leaf_co_splits[o_index];
+      Hatrix::Matrix& D = A.D(node, node, level);
+      int64_t c_size = D.rows - A.rank;
+
+      //Multiply with Uf^T
+      Hatrix::Matrix b_node(D.rows, 1);
+      auto b_node_splits = b_node.split(vec{c_size}, vec());
+      b_node_splits[0] = b_node_c;
+      b_node_splits[1] = b_node_o;
+      Hatrix::Matrix b_node_copy(b_node);
+      Hatrix::matmul(A.Uf(node, level), b_node_copy, b_node,
+		     true, false, 1., 0.);
+      b_node_c = b_node_splits[0];
+      b_node_o = b_node_splits[1];
+      
+      // Solve triangular from partial factorization
+      if(c_size > 0) {
+	auto L_splits = D.split(vec{c_size}, vec{c_size});
+	Hatrix::Matrix& Lcc = L_splits[0];
+	Hatrix::Matrix& Loc = L_splits[2];
+	Hatrix::solve_triangular(Lcc, b_node_c,
+				 Hatrix::Left, Hatrix::Lower, true);
+	Hatrix::matmul(Loc, b_node_c, b_node_o,
+		       false, false, -1, 1);
+      }
+    }
+    if(level == 1) { //Solve from level 1 oo full factorization
+      Hatrix::Matrix b_level_o(A.D(0, 0, 0).rows, 1);
+      auto b_level_o_splits = b_level_o.split(2, 1);
+      for(int64_t node = 0; node < num_nodes; node++) {
+	int64_t o_index = splits_per_node * (node + 1) - 1;
+	b_level_o_splits[node] = b_leaf_co_splits[o_index];
+      }
+      Hatrix::solve_triangular(A.D(0, 0, 0), b_level_o,
+			       Hatrix::Left, Hatrix::Lower, true);
+      for(int64_t node = 0; node < num_nodes; node++) {
+	int64_t o_index = splits_per_node * (node + 1) - 1;
+	b_leaf_co_splits[o_index] = b_level_o_splits[node];
+      }
+    }
+  }
+  
+  //---Solve Diagonal---
+  for(int64_t level = 1; level <= A.height; level++) {
+    int64_t num_nodes = pow(2, level);
+    int64_t splits_per_node = pow(2, A.height - level + 1);
+    if(level == 1) { //Solve from level 1 oo full factorization
+      Hatrix::Matrix b_level_o(A.D(0, 0, 0).rows, 1);
+      auto b_level_o_splits = b_level_o.split(2, 1);
+      for(int64_t node = 0; node < num_nodes; node++) {
+	int64_t o_index = splits_per_node * (node + 1) - 1;
+	b_level_o_splits[node] = b_leaf_co_splits[o_index];
+      }
+      Hatrix::solve_diagonal(A.D(0, 0, 0), b_level_o, Hatrix::Left);
+      for(int64_t node = 0; node < num_nodes; node++) {
+	int64_t o_index = splits_per_node * (node + 1) - 1;
+	b_leaf_co_splits[o_index] = b_level_o_splits[node];
+      }
+    }
+    for(int64_t node = 0; node < num_nodes; node++) {
+      Hatrix::Matrix& D = A.D(node, node, level);
+      int64_t c_size = D.rows - A.rank;
+      int64_t o_index = splits_per_node * (node + 1) - 1;
+      int64_t c_index = o_index - (splits_per_node/2);
+      Hatrix::Matrix& b_node_c = b_leaf_co_splits[c_index];
+      // Solve diagonal from partial factorization
+      if(c_size > 0) {
+	auto L_splits = D.split(vec{c_size}, vec{c_size});
+	Hatrix::Matrix& Lcc = L_splits[0];
+	Hatrix::solve_diagonal(Lcc, b_node_c, Hatrix::Left);
+      }
+    }
+  }
+
+  //---Backward Substitution---
+  for(int64_t level = 1; level <= A.height; level++) {
+    int64_t num_nodes = pow(2, level);
+    int64_t splits_per_node = pow(2, A.height - level + 1);
+    if(level == 1) { //Solve from level 1 oo full factorization
+      Hatrix::Matrix b_level_o(A.D(0, 0, 0).rows, 1);
+      auto b_level_o_splits = b_level_o.split(2, 1);
+      for(int64_t node = 0; node < 2; node++) {
+	int64_t o_index = splits_per_node * (node + 1) - 1;
+	b_level_o_splits[node] = b_leaf_co_splits[o_index];
+      }
+      Hatrix::solve_triangular(A.D(0, 0, 0), b_level_o,
+			       Hatrix::Left, Hatrix::Lower, true, true);
+      for(int64_t node = 0; node < 2; node++) {
+	int64_t o_index = splits_per_node * (node + 1) - 1;
+	b_leaf_co_splits[o_index] = b_level_o_splits[node];
+      }
+    }
+    for(int64_t node = 0; node < num_nodes; node++) {
+      //Get c and o slices of the vector to be operated
+      int64_t o_index = splits_per_node * (node + 1) - 1;
+      int64_t c_index = o_index - (splits_per_node/2);
+      Hatrix::Matrix& b_node_c = b_leaf_co_splits[c_index];
+      Hatrix::Matrix& b_node_o = b_leaf_co_splits[o_index];
+      Hatrix::Matrix& D = A.D(node, node, level);
+      int64_t c_size = D.rows - A.rank;
+
+      // Solve triangular from partial factorization
+      if(c_size > 0) {
+	auto L_splits = D.split(vec{c_size}, vec{c_size});
+	Hatrix::Matrix& Lcc = L_splits[0];
+	Hatrix::Matrix& Loc = L_splits[2];
+	Hatrix::matmul(Loc, b_node_o, b_node_c, true, false, -1., 1.);
+	Hatrix::solve_triangular(Lcc, b_node_c,
+				 Hatrix::Left, Hatrix::Lower, true, true);
+      }
+      
+      //Multiply with Uf
+      Hatrix::Matrix b_node(D.rows, 1);
+      auto b_node_splits = b_node.split(vec{c_size}, vec());
+      b_node_splits[0] = b_node_c;
+      b_node_splits[1] = b_node_o;
+      Hatrix::Matrix b_node_copy(b_node);
+      Hatrix::matmul(A.Uf(node, level), b_node_copy, b_node,
+		     false, false, 1., 0.);
+      b_node_c = b_node_splits[0];
+      b_node_o = b_node_splits[1];
+    }
+  }
+}
+
+double rel_error(const Hatrix::Matrix& A, const Hatrix::Matrix& B) {
+  double norm = Hatrix::norm(A);
+  double diff = Hatrix::norm(A - B);
+  return diff/norm;
+}
+
 int main(int argc, char** argv) {
   int64_t N = argc > 1 ? atoi(argv[1]) : 256;
   int64_t rank = argc > 2 ? atoi(argv[2]) : 8;
-  int64_t height = 2;
+  int64_t height = argc > 3 ? atoi(argv[3]) : 2;
 
   if (N % int(pow(2, height)) != 0 || rank > int(N / pow(2, height))) {
     std::cout << N << " % " << pow(2, height) << " != 0 || rank > leaf(" << int(N / pow(2, height))  << ")\n";
@@ -357,13 +439,20 @@ int main(int argc, char** argv) {
 
   Hatrix::Context::init();
   randvec_t randpts;
-  randpts.push_back(equally_spaced_vector(N, 0.0, 1.0)); // 1D
-
-  Hatrix::HSS A(randpts, N, rank, height);
-  factorize(A);
+  randpts.push_back(equally_spaced_vector(N, 0.0, 10.0)); // 1D
+  Hatrix::HSS_SPD A(randpts, N, rank, height);
+  double construction_error = A.construction_rel_error(randpts);
   
+  Hatrix::Matrix A_dense = Hatrix::generate_laplacend_matrix(randpts, N, N, 0, 0);
+  Hatrix::Matrix x = Hatrix::generate_random_matrix(N, 1);
+  Hatrix::Matrix b = A_dense * x;
+  factorize(A);
+  solve(A, b);
+  double solve_error = rel_error(x, b);
+
   std::cout <<"N=" <<N <<", rank=" <<rank <<", height=" <<height;
-  std::cout <<", construction error=" <<A.construction_rel_error(randpts) <<"\n";
+  std::cout <<", construction error=" <<construction_error;
+  std::cout <<", solve error=" <<solve_error <<"\n";
 
   Hatrix::Context::finalize();
   return 0;
