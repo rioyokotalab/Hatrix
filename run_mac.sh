475dca660cc249b24da1f5b4783fa533d9fd46c0
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2022-02-22 18:33:10 -0500

    better workins

--- run_mac.sh
+++ run_mac.sh
@@ -9,9 +9,16 @@ make -j all
 echo "---- SQ EXP NDIM=2 ADMIS=1 KERNEL FUNC=1 ----"
 
 # ./bin/UMV_H2_far_dense 240 25 30 0.6 2 geometry_admis 2 0
-./bin/UMV_H2_far_dense 2000 25 50 0.4 2 geometry_admis 2 0
-./bin/UMV_H2_far_dense 4000 25 50 0.4 2 geometry_admis 2 0
+# ./bin/UMV_H2_far_dense 2000 25 50 0.4 2 geometry_admis 2 0
+# ./bin/UMV_H2_far_dense 4000 25 50 0.4 2 geometry_admis 2 0
 # ./bin/UMV_H2_far_dense 1920 20 30 1.6 3 geometry_admis 0 0
 
 
 # ./bin/UMV_H2_far_dense 64 5 8 1 2 geometry_admis 0 0
+for rank in 6 7 8 10 14; do
+    ./bin/UMV_H2_far_dense 240 $rank 30 1.3 2 geometry_admis 0 0
+done
+
+for rank in 6 7 8 10 14; do
+    ./bin/UMV_H2_far_dense 240 $rank 30 1.3 3 geometry_admis 0 0
+done
