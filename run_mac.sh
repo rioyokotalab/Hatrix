64348042d1719916031f7dcc7751a644d1ab9479
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2022-03-02 20:44:04 -0500

    remove useless coments and code

--- run_mac.sh
+++ run_mac.sh
@@ -45,17 +45,21 @@ cat result.txt
 #     ./bin/UMV_H2_far_dense 1024 $rank 64 0.7 1 geometry_admis 1 0
 # done
 
-echo "STARSH GRID GEOMETRY DIM=2"
-./bin/UMV_H2_far_dense 512 15 32 1.2 3 geometry_admis 0 0
-# ./bin/UMV_H2_far_dense 1024 30 64 0.5 2 geometry_admis 1 0
-# for rank in 20 24 30; do
-#     ./bin/UMV_H2_far_dense 1024 $rank 64 0.5 2 geometry_admis 1 0
-# done
+echo "STARSH GRID DIAGONAL H2 DIM=2"
+for rank in 20 24 30; do
+    ./bin/UMV_H2_far_dense 1024 $rank 64 2 2 diagonal_admis 1 1
+done
 
-# echo "STARSH GRID GEOMETRY DIM=3"
-# for rank in 40 50 60; do
-#     ./bin/UMV_H2_far_dense 1024 $rank 64 1 3 geometry_admis 1 0
-# done
+
+echo "STARSH GRID GEOMETRY DIM=2"
+for rank in 20 24 30; do
+    ./bin/UMV_H2_far_dense 1024 $rank 64 0.7 2 geometry_admis 1 1
+done
+
+echo "STARSH GRID GEOMETRY DIM=3"
+for rank in 40 50 60; do
+    ./bin/UMV_H2_far_dense 1024 $rank 64 1 3 geometry_admis 1 0
+done
 
 # for rank in 40 50 60; do
 #     ./bin/UMV_H2_far_dense 1024 $rank 64 0.8 3 geometry_admis 1 0
