75070435b05a0f3a707b49cf820fd965cb31d4c0
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2022-04-09 15:22:26 -0400

    option parsing

--- examples/HSS/HSS_main.cpp
+++ examples/HSS/HSS_main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <cmath>
 #include <cassert>
+#include <cstdlib>
+#include <cstdio>
 #include <chrono>
 #include <fstream>
 #include <algorithm>
@@ -16,6 +18,11 @@ bool help_option_exists(std::vector<std::string>& cmd_options) {
   return (std::find(begin, end, "-h") != end) || (std::find(begin, end, "--help") != end);
 }
 
+constexpr int INIT_VALUE = -1;
+enum KIND_OF_PROBLEM {LAPLACE, SQR_EXP, SINE};
+enum KIND_OF_GEOMETRY {GRID, CIRCULAR};
+enum ADMIS_KIND {DIAGONAL, GEOMETRY};
+enum CONSTRUCT_ALGORITHM {MIRO, ID_RANDOM};
 
 int main(int argc, char* argv[]) {
   Hatrix::Context::init();
@@ -24,8 +31,6 @@ int main(int argc, char* argv[]) {
   for (int i = 0; i < argc; ++i) {
     cmd_options.push_back(std::string(argv[i]));
   }
-  const int width = 40;
-  const char separator = '*';
 
   if (help_option_exists(cmd_options)) {
     std::cout << "FRANKLIN help screen. Thank you for using FRANKLIN! May all your factorizations be linear." << std::endl;
@@ -41,12 +46,107 @@ int main(int argc, char* argv[]) {
               << "    circular -- Random unit circular surface. ndim determines whether line, circle or sphere." << std::endl
               << " --ndim :: Number of dimensions of the problem. 1, 2 or 3" << std::endl
               << " --rank :: Specify the maximum rank for a fixed rank calculation." << std::endl
+              << " --acc :: Specify the accuracy of the calculation. The --rank option is ignored if this is specified." << std::endl
               << " --admis :: Specify the admissibility constant. Double precision number >= 0." << std::endl
               << " --admis_kind :: Specify whether geometry-based admissiblity or diagonal-based: " << std::endl
               << "    geometry :: Admissiblity condition based on the geometry of the problem." << std::endl
               << "    diagonal :: Admissibility condition based on the distance from the diagonal."
+              << " --construct-algorithm :: Specify the kind of construction algorithm." << std::endl
+              << "    miro       -- From the miro board. Use SVD everywhere." << std::endl
+              << "    id_random  -- From Martinsson2010. Use ID + randomized sampling."
               << std::endl;
   }
+
+  long long N = 1000;
+  long long nleaf = 100;
+  KIND_OF_PROBLEM kind_of_problem = LAPLACE;
+  KIND_OF_GEOMETRY kind_of_geometry = GRID;
+  int ndim = 1;
+  int rank = 10;
+  double admis = 1;
+  double acc = -1;
+  ADMIS_KIND admis_kind = DIAGONAL;
+  CONSTRUCT_ALGORITHM construct_algorithm = MIRO;
+
+  for (auto iter = cmd_options.begin(); iter != cmd_options.end(); ++iter) {
+    auto option = *iter;
+
+    if (option == "--N") {
+      N = std::stol(*(++iter));
+    }
+    else if (option == "--nleaf") {
+      nleaf = std::stol(*(++iter));
+    }
+    else if (option == "--kind-of-problem") {
+      auto value = *(++iter);
+      if (value == "laplace") {
+        kind_of_problem = LAPLACE;
+      }
+      else if (value == "sqrexp") {
+        kind_of_problem = SQR_EXP;
+      }
+      else if (value == "sin") {
+        kind_of_problem = SINE;
+      }
+      else {
+        std::cout << "wrong value for --kind-of-problem " << value << std::endl;
+        abort();
+      }
+    }
+    else if (option == "--kind-of-geometry") {
+      auto value = *(++iter);
+      if (value == "grid") {
+        kind_of_geometry = GRID;
+      }
+      else if (value == "circular") {
+        kind_of_geometry = CIRCULAR;
+      }
+      else {
+        std::cout << "wrong value for --kind-of-geometry " << value << std::endl;
+        abort();
+      }
+    }
+    else if (option == "--ndim") {
+      ndim = std::stoi(*(++iter));
+    }
+    else if (option == "--rank") {
+      rank = std::stoi(*(++iter));
+    }
+    else if (option == "--acc") {
+      acc = std::stod(*(++iter));
+    }
+    else if (option == "--admis") {
+      admis = std::stod(*(++iter));
+    }
+    else if (option == "--admis-kind") {
+      auto value = *(++iter);
+      if (value == "geometry") {
+        admis_kind = GEOMETRY;
+      }
+      else if (value == "diagonal") {
+        admis_kind = DIAGONAL;
+      }
+      else {
+        std::cout << "wrong value for --admis-kind " << value << std::endl;
+        abort();
+      }
+    }
+    else if (option == "--construct-algorithm") {
+      auto value = *(++iter);
+      if (value == "miro") {
+        construct_algorithm = MIRO;
+      }
+      else if (value == "id_random") {
+        construct_algorithm = ID_RANDOM;
+      }
+      else {
+        std::cout << "wrong value for --construct-algorithm " << value << std::endl;
+        abort();
+      }
+    }
+  }
+
+  std::cout << "N: " << N << " leaf: " << nleaf << std::endl;
 
   Hatrix::Context::finalize();
   return 0;
--- run_mac.sh
+++ run_mac.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 make -j all
+./bin/HSS_main --N 800 --nleaf 40
 
 # ./bin/UMV_weak_Nlevel 320 15 4
 # ./bin/UMV_weak_Nlevel 640 15 5
@@ -12,40 +13,9 @@ make -j all
 # ./bin/UMV_H2_far_dense 512 12 64 1 3 diagonal_admis 0 1
 
 # echo "------ UMV H2 NLEVEL ------"
-for matrix_type in 1; do
-    echo "CIRCLE GEOMETRY"
-    for rank in 20 34 40; do
-        ./bin/UMV_H2_far_dense 2048 $rank 128 0 1 diagonal_admis 0 $matrix_type
-    done
-done
-
-#     echo "SPHERE GEOMETRY"
-#     for rank in 34 40; do
-#         ./bin/UMV_H2_far_dense 1024 $rank 64 1.2 3 geometry_admis 0 $matrix_type
-#     done
-
-#     echo "STARSH GRID DIAGONAL H2 DIM=2"
-#     for rank in 20 24 30; do
-#         ./bin/UMV_H2_far_dense 1024 $rank 64 2 2 diagonal_admis 1 $matrix_type
-#     done
-
-
-    # echo "STARSH GRID GEOMETRY DIM=2"
-    # for rank in 24; do
-    #     ./bin/UMV_H2_far_dense 512 $rank 64 0.7 2 geometry_admis 1 $matrix_type
-    # done
-
-#     echo "STARSH GRID GEOMETRY DIM=3"
-#     for rank in 40 50 60; do
-#         ./bin/UMV_H2_far_dense 1024 $rank 64 1 3 geometry_admis 1 $matrix_type
-#     done
-
-#     echo "STARSH SIN KERNEL DIM=2"
-#     for rank in 20 24 30 50; do
-#         ./bin/UMV_H2_far_dense 1024 $rank 64 0.4 2 geometry_admis 2 $matrix_type
+# for matrix_type in 1; do
+#     echo "CIRCLE GEOMETRY"
+#     for rank in 20 34 40; do
+#         ./bin/UMV_H2_far_dense 2048 $rank 128 0 1 diagonal_admis 0 $matrix_type
 #     done
 # done
-
-
-
-# cat result.txt
