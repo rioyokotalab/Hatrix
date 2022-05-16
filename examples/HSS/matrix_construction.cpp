196623825dda67ddad2c5eed48bbb35fba78becd
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2022-05-16 14:21:34 +0900

    generate leaf node function

--- examples/HSS/matrix_construction.cpp
+++ examples/HSS/matrix_construction.cpp
@@ -1,5 +1,6 @@
 #include <exception>
 
+#include "functions.hpp"
 #include "matrix_construction.hpp"
 #include "SymmetricSharedBasisMatrix.hpp"
 #include "Domain.hpp"
@@ -59,14 +60,39 @@ static void diagonal_admis_init(SymmetricSharedBasisMatrix& A, const Args& opts,
 void init_diagonal_admis(SymmetricSharedBasisMatrix& A, const Args& opts) {
   A.height = int64_t(log2(opts.N / opts.nleaf));
   diagonal_admis_init(A, opts, A.height);
+  std::reverse(std::begin(A.level_blocks), std::end(A.level_blocks));
+  A.is_admissible.insert(0, 0, 0, false);
 }
 
 void init_geometry_admis(SymmetricSharedBasisMatrix& A, const Args& opts) {
   throw std::exception();
 }
 
+static void
+generate_leaf_nodes(const Domain& domain,
+                    SymmetricSharedBasisMatrix& A,
+                    const Matrix& dense,
+                    const Matrix& rand) {
+  int64_t nblocks = A.level_blocks[A.height];
+  auto dense_splits = dense.split(nblocks, nblocks);
+
+  for (int64_t i = 0; i < nblocks; ++i) {
+    for (int64_t j = 0; j < nblocks; ++j) {
+      if (A.is_admissible.exists(i, j, A.height) &&
+          !A.is_admissible(i, j, A.height)) {
+        Matrix Aij(dense_splits[i * nblocks + j], true);
+        A.D.insert(i, j, A.height, std::move(Aij));
+      }
+    }
+  }
+}
+
 void construct_h2_matrix_miro(SymmetricSharedBasisMatrix& A,
                               const Domain& domain,
-                              const Args& args) {
+                              const Args& opts) {
+  int64_t P = 100;
+  Matrix dense = generate_p2p_matrix(domain, opts.kernel);
+  Matrix rand = generate_random_matrix(opts.N, P);
+  generate_leaf_nodes(domain, A, dense, rand);
 
 }
