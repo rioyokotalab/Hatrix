88d80d392c694ec36c053227c370640253e18c83
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2022-05-19 18:24:19 +0900

    start work on SLATE interface

--- examples/HSS/slate/HSS_slate.cpp
+++ examples/HSS/slate/HSS_slate.cpp
@@ -12,8 +12,22 @@
 
 #include "Hatrix/Hatrix.h"
 
+#include "mpi.h"
 #include <slate/slate.hh>
 
+#include "../Args.hpp"
+
+typedef struct MPISymmSharedBasisMatrix {
+
+} MPISymmSharedBasisMatrix;
+
 int main(int argc, char* argv[]) {
+  Args args(argc, argv);
+
+  int provided;
+  assert(MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided) == 0);
+
+  assert(MPI_Finalize() == 0);
+
   return 0;
 }
