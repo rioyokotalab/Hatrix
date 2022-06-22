2992cf6ab73e48767d2692e141b3160a7011e807
wawando <muhammad.ridwan.ab@gmail.com>
2022-06-22 17:20:32 +0900

    Fixed bug in updating S in fixed_acc LDLT

--- examples/H2_fixed_acc_LDL.cpp
+++ examples/H2_fixed_acc_LDL.cpp
@@ -1235,7 +1235,7 @@ void SymmetricH2::factorize_level(const Domain& domain,
         // Update S along the column
         for (int i = 0; i < nblocks; ++i) {
           if (is_admissible.exists(i, block, level) && is_admissible(i, block, level)) {
-            Matrix Sbar_i_block = matmul(S(i, block, level), r(block));
+            Matrix Sbar_i_block = matmul(S(i, block, level), r(block), false, true);
             if (F.exists(i, block)) {
               if (F(i, block).rows == U(i, level).cols && F(i, block).cols == block_size) {
                 Sbar_i_block += matmul(F(i, block), U(block, level));
