870478d6051801d144a4260aae7b360ed002fa0c
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2022-07-01 18:30:17 +0300

    forward permutetation with cholesky

--- examples/franklin/HSS/operations.cpp
+++ examples/franklin/HSS/operations.cpp
@@ -107,8 +107,36 @@ solve_forward_level(const SymmetricSharedBasisMatrix& A, Matrix& x_level,
 }
 
 static int64_t
-permute_forward(Matrix& x_level, int64_t level, int64_t permute_offset) {
+permute_forward(const SymmetricSharedBasisMatrix& A,
+                Matrix& x, int64_t level, int64_t permute_offset) {
+  Matrix copy(x);
+  int64_t num_nodes = pow(2, level);
+  int64_t c_offset = permute_offset;
+  for (int64_t block = 0; block < num_nodes; ++block) {
+    permute_offset += A.D(block, block, level).rows - A.ranks(block, level);
+  }
 
+  int64_t csize_offset = 0, bsize_offset = 0, rsize_offset = 0;
+  for (int64_t block = 0; block < num_nodes; ++block) {
+    int64_t rows = A.D(block, block, level).rows;
+    int64_t c_size = rows - A.ranks(block, level);
+
+    // copy the complement part of the vector into the temporary vector
+    for (int64_t i = 0; i < c_size; ++i) {
+      copy(c_offset + csize_offset + i, 0) = x(c_offset + bsize_offset + i, 0);
+    }
+    // copy the rank part of the vector into the temporary vector
+    for (int64_t i = 0; i < A.ranks(block, level); ++i) {
+      copy(permute_offset + rsize_offset + i, 0) =
+        x(c_offset + bsize_offset + c_size + i, 0);
+    }
+
+    csize_offset += c_size;
+    bsize_offset += rows;
+    rsize_offset += A.ranks(block, level);
+  }
+
+  x = copy;
   return permute_offset;
 }
 
@@ -117,15 +145,26 @@ Matrix
 solve(const SymmetricSharedBasisMatrix& A, const Matrix& b) {
   Matrix x(b);
   int64_t level_offset = 0;
-  int64_t level = A.max_level;
+  std::vector<Matrix> x_splits;
 
   // forward substitution.
-  // std::vector<Hatrix::Matrix> x_offset_views = x.split(std::vector<int64_t>(1, 0),
-  //                                                      {});
   for (int64_t level = A.max_level; level > A.min_level; --level) {
     int nblocks = pow(2, level);
-    // solve_forward_level(A, x_offset_views[1], level);
-    // level_offset = permute_forward(x, level, rhs_offset);
+    int64_t n = 0;              // total vector length due to variable ranks.
+    for (int64_t i = 0; i < nblocks; ++i) { n += A.D(i, i, level).rows; }
+
+    Matrix x_level(n, 1);
+    for (int64_t i = 0; i < x_level.rows; ++i) {
+      x_level(i, 0) = x(level_offset + i, 0);
+    }
+
+    solve_forward_level(A, x_level, level);
+
+    for (int64_t i = 0; i < x_level.rows; ++i) {
+      x(level_offset + i, 0) = x_level(i, 0);
+    }
+
+    level_offset = permute_forward(A, x, level, level_offset);
   }
 
   // last block forward
