42d9e09a7f66bd187c450a31a23abae3076d25a8
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2022-08-11 20:31:38 +0300

    switch order of compute

--- examples/franklin/HSS/operations.cpp
+++ examples/franklin/HSS/operations.cpp
@@ -350,12 +350,6 @@ matmul(const SymmetricSharedBasisMatrix& A, const Matrix& x) {
     int64_t child_level = level + 1;
     x_hat_offset -= pow(2, child_level);
 
-    std::cout << "bhat offset: " << b_hat_offset << std::endl;
-
-    for (int64_t row = 0; row < nblocks; ++row) {
-
-    }
-
     for (int64_t row = 0; row < nblocks; ++row) {
       int c_r1 = row * 2, c_r2 = row * 2 + 1;
 
@@ -364,11 +358,15 @@ matmul(const SymmetricSharedBasisMatrix& A, const Matrix& x) {
                          true,
                          false);
 
-      b_hat.push_back(bb);
       Matrix b_r2_cl = matmul(A.S(c_r2, c_r1, child_level),
                               x_hat[x_hat_offset + c_r1]);
 
+      b_hat.push_back(bb);
       b_hat.push_back(b_r2_cl);
+    }
+
+    for (int64_t row = 0; row < nblocks; ++row) {
+      int c_r1 = row * 2, c_r2 = row * 2 + 1;
 
       Matrix Ub = matmul(A.U(row, level),
                          b_hat[b_hat_offset + row]);
