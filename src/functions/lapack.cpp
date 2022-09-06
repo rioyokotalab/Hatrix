52e4221a47c3157d4c62c28c397fba41b927c535
wawando <muhammad.ridwan.ab@gmail.com>
2022-09-06 12:21:43 +0900

    Removed unnecessary check in fixed accuracy interpolative decomposition

--- src/functions/lapack.cpp
+++ src/functions/lapack.cpp
@@ -521,12 +521,6 @@ std::tuple<Matrix, std::vector<int64_t>, int64_t> error_interpolate(Matrix& A, d
   LAPACKE_dgeqp3(LAPACK_COL_MAJOR, A.rows, A.cols, &A, A.stride, jpvt.data(), tau.data());
 
   int64_t min_dim = A.min_dim();
-  if (std::abs(A(min_dim-1, min_dim-1)) > error) {
-    throw std::runtime_error("ID failed since the requested error cannot be reached. Min. error= " +
-                             std::to_string(std::abs(A(min_dim-1, min_dim-1))) + ", requested error= " +
-                             std::to_string(error));
-  }
-
   int64_t rank = 1;
   // find the right rank for this.
   for (int64_t i = 1; i < min_dim; ++i) {
