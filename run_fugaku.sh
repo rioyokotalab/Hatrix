5c7d71ebc13fb162a7eaeb17f3a2a2b71e17bd1a
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2022-12-03 00:03:48 +0900

    compile parsec

--- run_fugaku.sh
+++ run_fugaku.sh
@@ -19,17 +19,25 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/vol0003/hp190122/u01594/gitrepos/parsec
 make -j H2_main
 make -j H2_dtd
 
+
 for adm in 0.8; do
-    ./bin/H2_main --N 512 \
-         --nleaf 128 \
-         --kernel_func laplace \
-         --kind_of_geometry circular \
-         --ndim 2 \
-         --max_rank 50 \
-         --accuracy 1e-11 \
-         --admis $adm \
-         --admis_kind geometry \
-         --construct_algorithm miro \
-         --add_diag 1e-7 \
-         --use_nested_basis
+    nleaf=512
+    ndim=2
+    max_rank=110
+
+    for N in 4096; do
+	    echo "running"
+        mpirun -n 1 ./bin/H2_dtd --N $N \
+                      --nleaf $nleaf \
+                      --kernel_func laplace \
+                      --kind_of_geometry grid \
+                      --ndim $ndim \
+                      --max_rank $max_rank \
+                      --accuracy 1e-8 \
+                      --admis $adm \
+                      --admis_kind geometry \
+                      --construct_algorithm miro \
+                      --add_diag 1e-10 \
+                      --use_nested_basis
+    done
 done
