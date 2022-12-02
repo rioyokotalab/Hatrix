56fb2c07bdc0743380a6e170576fa3a20e468011
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2022-12-03 00:11:48 +0900

    first pasc test

---
+++ scripts/pasc/awesome.sh
@@ -0,0 +1,58 @@
+#!/bin/sh -x
+
+#PJM -L "node=64"
+#PJM -L "rscunit=rscunit_ft01"
+#PJM -L "rscgrp=small"
+#PJM -L "elapse=24:00:00"
+#PJM -L "freq=2200"
+#PJM -L "throttling_state=0"
+#PJM -L "issue_state=0"
+#PJM -L "ex_pipe_state=0"
+#PJM -L "eco_state=0"
+#PJM --mpi "proc=64"
+#PJM --mpi "max-proc-per-node=1"
+#PJM -s
+
+export PARALLEL=1
+export OMP_NUM_THREADS=1
+export PLE_MPI_STD_EMPTYFILE=off
+export FLIB_SCCR_CNTL=FALSE
+export FLIB_PTHREAD=1
+
+export OMP_PLACES=cores
+export OMP_DISPLAY_AFFINITY=TRUE
+export OMP_PROC_BIND=close
+export OMP_BIND=close
+export OMP_NUM_THREADS=1
+export XOS_MMM_L_PAGING_POLICY="demand:demand:demand"
+
+source /vol0004/apps/oss/spack/share/spack/setup-env.sh
+
+source ~/.bashrc
+
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/hp190122/u01594/gitrepos/googletest/build/lib64/pkgconfig:/vol0003/hp190122/u01594/gitrepos/lorapo/stars-h-rio/build/installdir/lib/pkgconfig
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/vol0003/hp190122/u01594/gitrepos/parsec/build/lib64/pkgconfig
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/vol0003/hp190122/u01594/gitrepos/parsec/build/lib64
+
+make -j H2_dtd
+
+for adm in 0.8; do
+    nleaf=512
+    ndim=2
+    max_rank=110
+
+    for N in 262144; do
+        mpiexec -stdout out_64.log -stderr err_64.log ./bin/H2_dtd --N $N \
+                      --nleaf $nleaf \
+                      --kernel_func laplace \
+                      --kind_of_geometry grid \
+                      --ndim $ndim \
+                      --max_rank $max_rank \
+                      --accuracy 1e-8 \
+                      --admis $adm \
+                      --admis_kind geometry \
+                      --construct_algorithm miro \
+                      --add_diag 1e-10 \
+                      --use_nested_basis
+    done
+done
