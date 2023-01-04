c325c2f6a764e60a4757697a367cec27f6a5d887
wawando <muhammad.ridwan.ab@gmail.com>
2023-01-04 15:00:56 +0900

    Fix bug on nearfield sampling

--- examples/common/Domain.hpp
+++ examples/common/Domain.hpp
@@ -453,6 +453,7 @@ class Domain {
 
   void build_interactions(const double theta) {
     dual_tree_traversal(cells[0], cells[0], theta);
+    sort_interaction_lists();
   }
 
   /*
@@ -558,18 +559,16 @@ class Domain {
       const auto level_ncells = (int64_t)1 << level;
       const auto level_offset = level_ncells - 1;
       for (int64_t node = 0; node < level_ncells; node++) {
-        auto& cell = cells[level_offset + node];
-        int64_t near_nbodies = 0;
-        for (const auto near_idx: cell.near_list) {
-          near_nbodies += cells[near_idx].sample_bodies.size();
-        }
+        const auto cell_idx = level_offset + node;
+        auto& cell = cells[cell_idx];
         std::vector<int64_t> initial_sample;
-        initial_sample.reserve(near_nbodies);
         for (const auto near_idx: cell.near_list) {
           const auto& near_cell = cells[near_idx];
-          initial_sample.insert(initial_sample.end(),
-                                near_cell.sample_bodies.begin(),
-                                near_cell.sample_bodies.end());
+          if ((near_cell.level == cell.level) && (near_idx != cell_idx)) {
+            initial_sample.insert(initial_sample.end(),
+                                  near_cell.sample_bodies.begin(),
+                                  near_cell.sample_bodies.end());
+          }
         }
         cell.sample_nearfield =
             select_sample_bodies(ndim, bodies, initial_sample,
