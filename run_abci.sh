26b158ac542d27aa3c8088352faf08b62c27f5e1
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2023-01-14 18:25:06 +0900

    selective gprof profiling for tasks

--- run_abci.sh
+++ run_abci.sh
@@ -9,7 +9,7 @@
 source ~/.bashrc
 
 module purge
-module load intel-mpi/2021.5 gcc intel-mkl/2022.0.0 cmake/3.22.3
+module load intel-mpi/2021.5 gcc intel-mkl/2022.0.0 cmake/3.22.3 intel-vtune intel-itac
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/acb10922qh/gitrepos/parsec/build/lib64/pkgconfig:/home/acb10922qh/gitrepos/googletest/build/lib64/pkgconfig
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/acb10922qh/gitrepos/parsec/build/lib64
@@ -19,32 +19,37 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/acb10922qh/gitrepos/parsec/build/l
 
 export OMP_PLACES=cores
 # export OMP_NUM_THREADS=1
+# mpirun -n 4 -genv I_MPI_DEBUG=10  xterm -e gdb -ex=run --args ./bin/H2_dtd --N $N \
+# mpirun -n 1  -gtool "gdb:0=attach" ./bin/H2_dtd --N $N \
+# mpirun -n 1 -gtool "gdb:0=attach" ./bin/H2_dtd --N $N \
 
-make -j H2_dtd
+# mpiexec.hydra -n 2 -genv I_MPI_BIND_NUMA=0,1 xterm -e gdb -ex=run --args ./bin/H2_dtd --N $N \
+make -j H2_dtd VERBOSE=1
 
+export GMON_OUT_PREFIX=gmon.out-
 
 for adm in 0.8; do
     nleaf=256
     ndim=3
     max_rank=150
 
-    for N in 4096; do
-        # mpirun -n 4 -genv I_MPI_DEBUG=10  xterm -e gdb -ex=run --args ./bin/H2_dtd --N $N \
-            # mpirun -n 1  -gtool "gdb:0=attach" ./bin/H2_dtd --N $N \
-            # mpirun -n 1 -gtool "gdb:0=attach" ./bin/H2_dtd --N $N \
+    for N in 8192; do
 
-            # mpiexec.hydra -n 2 -genv I_MPI_BIND_NUMA=0,1 xterm -e gdb -ex=run --args ./bin/H2_dtd --N $N \
-        mpirun -n 2 -genv I_MPI_BIND_NUMA=0,1 ./bin/H2_dtd --N $N \
-               --nleaf $nleaf \
-               --kernel_func laplace \
-               --kind_of_geometry grid \
-               --ndim $ndim \
-               --max_rank $max_rank \
-               --accuracy 1e-8 \
-               --admis $adm \
-               --admis_kind geometry \
-               --construct_algorithm miro \
-               --add_diag 1e-10 \
-               --use_nested_basis
+        mpirun -n 2 \
+                                       ./bin/H2_dtd --N $N \
+                                       --nleaf $nleaf \
+    --kernel_func laplace \
+    --kind_of_geometry grid \
+    --ndim $ndim \
+    --max_rank $max_rank \
+    --accuracy 1e-8 \
+    --admis $adm \
+    --admis_kind geometry \
+    --construct_algorithm miro \
+    --add_diag 1e-10 \
+    --use_nested_basis
     done
 done
+
+gprof -s bin/H2_dtd gmon.out-*
+gprof -q bin/H2_dtd gmon.sum > out.gprof
--- scripts/pasc/abci/test.sh
+++ scripts/pasc/abci/test.sh
@@ -9,7 +9,7 @@
 source ~/.bashrc
 
 module purge
-module load intel-mpi/2021.5 gcc intel-mkl/2022.0.0 cmake/3.22.3
+module load intel-mpi/2021.5 gcc intel-mkl/2022.0.0 cmake/3.22.3 intel-vtune intel-itac
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/acb10922qh/gitrepos/parsec/build/lib64/pkgconfig:/home/acb10922qh/gitrepos/googletest/build/lib64/pkgconfig
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/acb10922qh/gitrepos/parsec/build/lib64
@@ -19,24 +19,32 @@ export OMP_PLACES=cores
 make clean
 make -j H2_dtd
 # mpirun -n $procs -ppn 2 -f $SGE_JOB_HOSTLIST
+                   # -gtool "vtune -collect hpc-performance -r vtune-mpi:all" \
+                       # -trace -trace-pt2pt \
+rm gmon.out-*
+export GMON_OUT_PREFIX=gmon.out-
 
 for adm in 0.8; do
-    	ndim=3
-    	max_rank=50
-	for nleaf in 1024 2048; do
-    		for N in 131072; do
-        		mpirun -n 16 -ppn 1 -f $SGE_JOB_HOSTLIST ./bin/H2_dtd --N $N \
-               			--nleaf $nleaf \
-               			--kernel_func laplace \
-               			--kind_of_geometry grid \
-               			--ndim $ndim \
-               			--max_rank $max_rank \
-               			--accuracy 1e-8 \
-               			--admis $adm \
-               			--admis_kind geometry \
-               			--construct_algorithm miro \
-               			--add_diag 1e-8 \
-               			--use_nested_basis
-    		done
-	done
+    ndim=3
+    max_rank=50
+    for nleaf in 1024 ; do
+    	for N in 131072; do
+            mpirun -n 16 -ppn 1 -f $SGE_JOB_HOSTLIST -l \
+                   ./bin/H2_dtd --N $N \
+               	   --nleaf $nleaf \
+               	   --kernel_func laplace \
+               	   --kind_of_geometry grid \
+               	   --ndim $ndim \
+               	   --max_rank $max_rank \
+               	   --accuracy 1e-8 \
+               	   --admis $adm \
+               	   --admis_kind geometry \
+               	   --construct_algorithm miro \
+               	   --add_diag 1e-8 \
+               	   --use_nested_basis
+    	done
+    done
 done
+
+gprof -s bin/H2_dtd gmon.out-*
+gprof -q bin/H2_dtd gmon.sum > NEW.gprof
