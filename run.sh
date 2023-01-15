ae717a5311963923f81d7ac73fefb7e51cbc2dd1
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2023-01-15 18:31:13 +0900

    openmpi mca param

--- run.sh
+++ run.sh
@@ -23,7 +23,8 @@ for adm in 0.8; do
     max_rank=50
 
     for N in 65536; do
-        mpirun -n 1 ./bin/H2_dtd --N $N \
+        mpirun --mca opal_warn_on_missing_libcuda 0 -n 1 \
+               ./bin/H2_dtd --N $N \
                       --nleaf $nleaf \
                       --kernel_func laplace \
                       --kind_of_geometry grid \
