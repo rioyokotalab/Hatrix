50a327c6fd752c161e3deeecac96b83ac8ed45fa
delta-leader <a0855909@unet.univie.ac.at>
2023-01-30 17:24:50 +0900

    templated gemm tests

--- test/matmul.cpp
+++ test/matmul.cpp
@@ -1,85 +1,141 @@
 #include <cstdint>
 #include <iomanip>
 #include <sstream>
+#include <string>
 #include <tuple>
+#include <vector>
 
 #include "Hatrix/Hatrix.h"
 #include "gtest/gtest.h"
 
-class MatMulTests
-    : public testing::TestWithParam<
-          std::tuple<int64_t, int64_t, int64_t, bool, bool, double, double>> {};
+template <typename DT>
+class GemmTests : public testing::Test {
+  protected:
+  // Matrix dimensions used in the tests
+  std::vector<int64_t> sizes = {16, 32};
+  std::vector<std::tuple<int64_t, int64_t, int64_t>> dims;
 
-TEST_P(MatMulTests, matmul) {
-  Hatrix::Context::init();
-  int64_t M, N, K;
-  bool transA, transB;
-  double alpha, beta;
-  std::tie(M, K, N, transA, transB, alpha, beta) = GetParam();
-  Hatrix::Matrix A =
-      Hatrix::generate_random_matrix(transA ? K : M, transA ? M : K);
-  Hatrix::Matrix B =
-      Hatrix::generate_random_matrix(transB ? N : K, transB ? K : N);
-  Hatrix::Matrix C = Hatrix::generate_random_matrix(M, N);
-  Hatrix::Matrix C_check(C);
-  Hatrix::matmul(A, B, C, transA, transB, alpha, beta);
-  Hatrix::Context::join();
+  // Parameters used in the tests
+  std::vector<bool> trans = {true, false};
+  std::vector<double> scalars = {-1, 0.5, 1};
+  std::vector<std::tuple<bool, bool, double, double>> params;
 
-  // Manual matmul
-  for (int64_t i = 0; i < M; ++i) {
-    for (int64_t j = 0; j < N; ++j) {
-      C_check(i, j) =
-          (beta * C_check(i, j) +
-           alpha * (transA ? A(0, i) : A(i, 0)) * (transB ? B(j, 0) : B(0, j)));
+  void SetUp() override {
+    for (size_t i = 0; i < trans.size(); ++i) {
+      for (size_t j = 0; j < trans.size(); ++j) {
+        for (size_t k = 0; k < scalars.size(); ++k) {
+          for (size_t l = 0; l < scalars.size(); ++l) {
+            params.push_back(
+              std::make_tuple(trans[i], trans[j], scalars[k], scalars[l]));
+          }
+        }
+      }
+    }
+    for (size_t i = 0; i < sizes.size(); ++i) {
+      for (size_t j = 0; j < sizes.size(); ++j) {
+        for (size_t k = 0; k < sizes.size(); ++k) {
+          dims.push_back(std::make_tuple(sizes[i], sizes[j], sizes[k]));
+        }
+      }
+    }
+  }
+};
+
+// templated function to compare floats and doubles respectively
+template <typename DT>
+void inline expect_fp_eq(const DT a, const DT b, const std::basic_string<char>& err_msg) {
+  if (std::is_same<DT, double>::value){
+    EXPECT_NEAR(a, b, 10e-14) << err_msg;
+  }     
+  else {
+    EXPECT_NEAR(a, b, 10e-6) << err_msg;
+  }                                        
+}
+
+// templated function to compare float and double matrices respectively
+template <typename DT>
+void inline matrix_fp_eq(const Hatrix::Matrix<DT> A, const Hatrix::Matrix<DT> B, const std::basic_string<char>& err_msg) {
+  EXPECT_EQ(A.rows, B.rows);
+  EXPECT_EQ(A.cols, B.cols);
+  EXPECT_EQ(A.stride, B.stride);
+  for (int64_t i = 0; i < A.rows; ++i) {
+    for (int64_t j = 0; j < A.cols; ++j) {
+      expect_fp_eq(A(i, j), B(i, j), 
+      "Wrong value at index ["+std::to_string(i)+", "+std::to_string(j)
+      +"] " + err_msg);
+    }
+  }                                        
+}
+
+template <typename DT>
+void naive_matmul(const Hatrix::Matrix<DT>& A, const Hatrix::Matrix<DT>& B, Hatrix::Matrix<DT>& C,
+  const bool transA, const bool transB, const DT alpha, const DT beta) {
+  const int64_t K = transA ? A.rows : A.cols;
+  for (int64_t i = 0; i < C.rows; ++i) {
+    for (int64_t j = 0; j < C.cols; ++j) {
+      C(i, j) = (beta * C(i, j) +
+        alpha * (transA ? A(0, i) : A(i, 0)) * (transB ? B(j, 0) : B(0, j)));
       for (int64_t k = 1; k < K; ++k) {
-        C_check(i, j) += (alpha * (transA ? A(k, i) : A(i, k)) *
-                          (transB ? B(j, k) : B(k, j)));
+        C(i, j) += (alpha * (transA ? A(k, i) : A(i, k)) *
+          (transB ? B(j, k) : B(k, j)));
       }
     }
   }
-
-  // Check result
-  for (int64_t i = 0; i < M; ++i) {
-    for (int64_t j = 0; j < N; ++j) {
-      EXPECT_NEAR(C_check(i, j), C(i, j), 10e-14);
-    }
-  }
-  Hatrix::Context::finalize();
 }
 
-TEST_P(MatMulTests, matmulReturn) {
-  Hatrix::Context::init();
-  int64_t M, N, K;
-  bool transA, transB;
-  double alpha, _;
-  std::tie(M, K, N, transA, transB, alpha, _) = GetParam();
-  Hatrix::Matrix A =
-      Hatrix::generate_random_matrix(transA ? K : M, transA ? M : K);
-  Hatrix::Matrix B =
-      Hatrix::generate_random_matrix(transB ? N : K, transB ? K : N);
-  Hatrix::Matrix C = Hatrix::matmul(A, B, transA, transB, alpha);
-  Hatrix::Context::join();
+// template types used in the tests
+using Types = ::testing::Types<float, double>;
+TYPED_TEST_SUITE(GemmTests, Types);
 
-  // Manual matmul
-  Hatrix::Matrix C_check(C.rows, C.cols);
-  for (int64_t i = 0; i < M; ++i) {
-    for (int64_t j = 0; j < N; ++j) {
-      for (int64_t k = 0; k < K; ++k) {
-        C_check(i, j) += (alpha * (transA ? A(k, i) : A(i, k)) *
-                          (transB ? B(j, k) : B(k, j)));
-      }
+TYPED_TEST(GemmTests, Gemm) {
+  for (auto const& [M, N, K] : this->dims) {
+    for (auto const& [transA, transB, alpha, beta] : this->params) {
+      Hatrix::Matrix<TypeParam> A =
+        Hatrix::generate_random_matrix<TypeParam>(transA ? K : M, transA ? M : K);
+      Hatrix::Matrix<TypeParam> B =
+        Hatrix::generate_random_matrix<TypeParam>(transB ? N : K, transB ? K : N);
+      Hatrix::Matrix<TypeParam> C = 
+        Hatrix::generate_random_matrix<TypeParam>(M, N);
+      Hatrix::Matrix<TypeParam> C_check(C);
+      Hatrix::matmul(A, B, C, transA, transB, alpha, beta);
+
+      naive_matmul(A, B, C_check, transA, transB, static_cast<TypeParam>(alpha), static_cast<TypeParam>(beta));
+
+      // Check result
+      matrix_fp_eq(C_check, C,
+        "Dims(m = "+std::to_string(M)+", k = "+std::to_string(K)+
+        ", n = "+std::to_string(N)+") Params(transA = "
+        +std::to_string(transA)+", transB = "+std::to_string(transB)
+        +", alpha = "+std::to_string(alpha)+", beta = "
+        +std::to_string(beta));
     }
   }
-
-  // Check result
-  for (int64_t i = 0; i < M; ++i) {
-    for (int64_t j = 0; j < N; ++j) {
-      EXPECT_NEAR(C_check(i, j), C(i, j), 10e-14);
-    }
-  }
-  Hatrix::Context::finalize();
 }
 
+TYPED_TEST(GemmTests, GemmReturn) {
+  for (auto const& [M, N, K] : this->dims) {
+    for (auto const& [transA, transB, alpha, beta] : this->params) {
+      Hatrix::Matrix<TypeParam> A =
+        Hatrix::generate_random_matrix<TypeParam>(transA ? K : M, transA ? M : K);
+      Hatrix::Matrix<TypeParam> B =
+        Hatrix::generate_random_matrix<TypeParam>(transB ? N : K, transB ? K : N);
+      Hatrix::Matrix<TypeParam> C = Hatrix::matmul(A, B, transA, transB, alpha);
+
+      Hatrix::Matrix<TypeParam> C_check(C.rows, C.cols);
+      naive_matmul(A, B, C_check, transA, transB, static_cast<TypeParam>(alpha), static_cast<TypeParam>(beta));
+
+      // Check result
+      matrix_fp_eq(C_check, C,
+        "Dims(m = "+std::to_string(M)+", k = "+std::to_string(K)+
+        ", n = "+std::to_string(N)+") Params(transA = "
+        +std::to_string(transA)+", transB = "+std::to_string(transB)
+        +", alpha = "+std::to_string(alpha)+", beta = "
+        +std::to_string(beta));
+    }
+  }
+}
+
+//TODO is this really a unit test?
 TEST(MatMulViewTests, matmulView) {
   Hatrix::Context::init();
   int64_t block = 100, sub_block = 25;
@@ -107,41 +163,3 @@ TEST(MatMulViewTests, matmulView) {
 
   Hatrix::Context::finalize();
 }
-
-INSTANTIATE_TEST_SUITE_P(
-    Params, MatMulTests,
-    testing::Combine(testing::Values(13), testing::Values(47),
-                     testing::Values(55), testing::Bool(), testing::Bool(),
-                     testing::Range(-1.0, 1.5, 0.5),
-                     testing::Range(-1.0, 1.5, 0.5)),
-    [](const testing::TestParamInfo<MatMulTests::ParamType>& info) {
-      std::stringstream alpha_;
-      std::stringstream beta_;
-      alpha_ << std::fixed << std::setprecision(1) << std::get<5>(info.param);
-      beta_ << std::fixed << std::setprecision(1) << std::get<6>(info.param);
-      std::string alpha = alpha_.str();
-      alpha.replace(alpha.find_last_of("."), 1, "l");
-      if (alpha.find_last_of("-") < alpha.length())
-        alpha.replace(alpha.find_last_of("-"), 1, "m");
-      std::string beta = beta_.str();
-      beta.replace(beta.find_last_of("."), 1, "l");
-      if (beta.find_last_of("-") < beta.length())
-        beta.replace(beta.find_last_of("-"), 1, "m");
-      std::string name =
-          ("TA" + std::to_string(std::get<3>(info.param)) + "TB" +
-           std::to_string(std::get<4>(info.param)) + "A" + alpha + "B" + beta);
-      return name;
-    });
-
-INSTANTIATE_TEST_SUITE_P(
-    Sizes, MatMulTests,
-    testing::Combine(testing::Values(16, 32, 64), testing::Values(16, 32, 64),
-                     testing::Values(16, 32, 64), testing::Values(false),
-                     testing::Values(false), testing::Values(1),
-                     testing::Values(1)),
-    [](const testing::TestParamInfo<MatMulTests::ParamType>& info) {
-      std::string name = ("M" + std::to_string(std::get<0>(info.param)) + "K" +
-                          std::to_string(std::get<1>(info.param)) + "N" +
-                          std::to_string(std::get<2>(info.param)));
-      return name;
-    });
