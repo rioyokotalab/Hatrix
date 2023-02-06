1fd763cffcf1791e0847f100dc32155fcadee318
delta-leader <a0855909@unet.univie.ac.at>
2023-02-06 12:28:07 +0900

    templated norm tests

--- test/norms.cpp
+++ test/norms.cpp
@@ -6,34 +6,51 @@
 #include "Hatrix/Hatrix.h"
 #include "gtest/gtest.h"
 
-class NormTests : public testing::TestWithParam<std::tuple<int64_t, int64_t>> {
-};
+template <typename DT>
+class NORMTests : public testing::Test {
+  protected:
+  // Matrix dimensions used in the tests
+  std::vector<int64_t> sizes = {1, 10, 20, 70, 99, 100};
+  std::vector<std::tuple<int64_t, int64_t>> dims;
 
-TEST_P(NormTests, norm) {
-  int64_t m, n;
-  std::tie(m, n) = GetParam();
-  Hatrix::Context::init();
-  Hatrix::Matrix A = Hatrix::generate_random_matrix(m, n);
-
-  double norm = 0;
-  for (int64_t j = 0; j < A.cols; ++j) {
-    for (int64_t i = 0; i < A.rows; ++i) {
-      norm += A(i, j) * A(i, j);
+  void SetUp() override {
+    for (size_t i = 0; i < sizes.size(); ++i) {
+      for (size_t j = 0; j < sizes.size(); ++j) {
+        dims.push_back(std::make_tuple(sizes[i], sizes[j]));
+      }
     }
   }
-  norm = std::sqrt(norm);
+};
 
-  EXPECT_FLOAT_EQ(norm, Hatrix::norm(A));
-  Hatrix::Context::finalize();
+// templated function to compare floats and doubles respectively
+template <typename DT>
+void inline expect_fp_eq(const DT a, const DT b, const std::basic_string<char>& err_msg) {
+  if (std::is_same<DT, double>::value){
+    EXPECT_NEAR(a, b, 10e-13) << err_msg;
+  }     
+  else {
+    EXPECT_NEAR(a, b, 10e-5) << err_msg;
+  }                                        
 }
 
-INSTANTIATE_TEST_SUITE_P(
-    LAPACK, NormTests,
-    testing::Values(std::make_tuple(100, 100), std::make_tuple(20, 70),
-                    std::make_tuple(99, 55), std::make_tuple(1, 10),
-                    std::make_tuple(13, 1)),
-    [](const testing::TestParamInfo<NormTests::ParamType>& info) {
-      std::string name = ("m" + std::to_string(std::get<0>(info.param)) + "n" +
-                          std::to_string(std::get<1>(info.param)));
-      return name;
-    });
+// template types used in the tests
+using Types = ::testing::Types<float, double>;
+TYPED_TEST_SUITE(NORMTests, Types);
+
+TYPED_TEST(NORMTests, Norm) {
+  for (auto const& [m, n] : this->dims) {
+    Hatrix::Matrix<TypeParam> A = Hatrix::generate_random_matrix<TypeParam>(m, n);
+
+    TypeParam norm = 0;
+    for (int64_t j = 0; j < A.cols; ++j) {
+      for (int64_t i = 0; i < A.rows; ++i) {
+        norm += A(i, j) * A(i, j);
+      }
+    }
+    norm = std::sqrt(norm);
+
+    expect_fp_eq(norm, Hatrix::norm(A),
+      "Values are different (" + std::to_string(m) + "x"
+      + std::to_string(n) + " matrix)");
+  }
+}
