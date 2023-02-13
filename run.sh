ce60a8f0e72f4c14a65a477d358dec733533c16e
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2023-02-13 12:23:57 +0900

    benchmarking

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#YBATCH -r epyc-7502_8
+#YBATCH -r dgx-a100_4
 #SBATCH -N 1
 #SBATCH -J PAPI
 #SBATCH --time=72:00:00
@@ -16,27 +16,27 @@ export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/sameer.deshmukh/gitrepos/parsec/bu
 #:/mnt/nfs/packages/x86_64/intel/2022/mpi/2021.6.0/lib/pkgconfig
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/sameer.deshmukh/gitrepos/parsec/build/lib:/mnt/nfs/packages/x86_64/cuda/cuda-11.7/lib64::/home/sameer.deshmukh/gitrepos/papi/src/lib
 
-make -j H2_dtd
+make -j H2_main
 
-for adm in 3; do
+for adm in 0.7 0.8 0.9 1.2 1.4 1.6 1.8; do
     nleaf=512
-    max_rank=30
+    max_rank=50
+    ndim=3
 
-    for ndim in 2; do
-        for N in 8192; do
-        mpirun -n 1 ./bin/H2_dtd --N $N \
-                      --nleaf $nleaf \
-                      --kernel_func laplace \
-                      --kind_of_geometry grid \
-                      --ndim $ndim \
-                      --max_rank $max_rank \
-                      --accuracy 1e-12 \
-                      --admis $adm \
-                      --admis_kind geometry \
-                      --construct_algorithm miro \
-                      --add_diag 1e-9 \
-                      --kind_of_recompression 3 \
-                      --use_nested_basis
-        done
+    for N in 131072; do
+        ./bin/H2_main --N $N \
+               --nleaf $nleaf \
+               --kernel_func laplace \
+               --kind_of_geometry grid \
+               --ndim $ndim \
+               --max_rank $max_rank \
+               --accuracy 1e-12 \
+               --admis $adm \
+               --admis_kind geometry \
+               --construct_algorithm miro \
+               --add_diag 1e-9 \
+               --kind_of_recompression 3 \
+               --use_nested_basis
+
     done
 done
