4a1445c3e8de899a51319b6cdbcae62ac8fe18e4
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2023-03-19 13:45:56 +0900

    prepare GSL on lab server

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#YBATCH -r dgx-a100_8
+#YBATCH -r epyc-7502_8
 #SBATCH -N 1
-#SBATCH -J BLR2
-#SBATCH --time=72:00:00
+#SBATCH -J SAMEH
+#SBATCH --time=24:00:00
 
 source ~/.bashrc
 source /home/sameer.deshmukh/gitrepos/parsec/build/bin/parsec.env.sh
@@ -11,7 +11,7 @@ source /etc/profile.d/modules.sh
 module purge
 module load cuda intel/2022/mkl gcc/10.4 cmake lapack/3.9.0 openmpi/4.0.5
 
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/sameer.deshmukh/gitrepos/parsec/build/lib/pkgconfig:/home/sameer.deshmukh/gitrepos/papi/src/lib/pkgconfig
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/sameer.deshmukh/gitrepos/parsec/build/lib/pkgconfig:/home/sameer.deshmukh/gitrepos/papi/src/lib/pkgconfig:/home/sameer.deshmukh/gitrepos/gsl-2.7.1/build/lib/pkgconfig
 
 #:/mnt/nfs/packages/x86_64/intel/2022/mpi/2021.6.0/lib/pkgconfig
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/sameer.deshmukh/gitrepos/parsec/build/lib:/mnt/nfs/packages/x86_64/cuda/cuda-11.7/lib64:/home/sameer.deshmukh/gitrepos/papi/src/lib
@@ -22,21 +22,18 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/sameer.deshmukh/gitrepos/parsec/bu
 # export OMP_PROC_BIND=close
 
 
-make -j H2_dtd
-# make -j H2_main
+make -j H2_main
 
-for adm in 1; do
-    nleaf=32
-    ndim=3
-    max_rank=16
+nleaf=256
+max_rank=50
+ndim=2
+adm=0
 
-    # gdb -q -iex "set auto-load safe-path /home/user/gdb" -ex run --args
-
-    for N in 65536; do
-        mpirun -tag-output --oversubscribe -n 64 \
-               ./bin/H2_dtd --N $N \
+for N in 40000; do
+    for p3 in 0.8 0.9 1; do
+        ./bin/H2_main --N $N \
                       --nleaf $nleaf \
-                      --kernel_func laplace \
+                      --kernel_func gsl_matern \
                       --kind_of_geometry grid \
                       --ndim $ndim \
                       --max_rank $max_rank \
@@ -44,8 +41,8 @@ for adm in 1; do
                       --admis $adm \
                       --admis_kind diagonal \
                       --construct_algorithm miro \
-                      --add_diag 1e-9  \
-                      --kind_of_recompression 3 \
-                      --use_nested_basis
+                      --geometry_file "/home/sameer.deshmukh/XY_40000_1" \
+                      --param_1 1e-4 --param_2 1 --param_3 $p3  \
+                      --kind_of_recompression 3
     done
 done
