98931d1e6b703fe7a204c38ce625e740519516b9
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2023-07-27 10:49:31 +0900

    add special type for scalapack matrix

--- examples/distributed/H2_construct/main.cpp
+++ examples/distributed/H2_construct/main.cpp
@@ -18,11 +18,55 @@
 
 using namespace Hatrix;
 
+static const int SCALAPACK_BLOCK_SIZE = 256;
+
+int indxl2g(int indxloc, int nb, int iproc, int nprocs) {
+  return nprocs * nb * ((indxloc - 1) / nb) +
+    (indxloc-1) % nb + ((nprocs + iproc) % nprocs) * nb + 1;
+}
+
+class ScaLAPACK_dist_matrix_t {
+  // scalapack storage for matrix descriptor.
+  std::vector<int> DESC;
+  std::vector<double> data;
+public:
+
+  int local_nrows, local_ncols;
+
+  ScaLAPACK_dist_matrix_t(int nrows, int ncols,
+                          int block_nrows, int block_ncols,
+                          int begin_prow, int begin_pcol,
+                          int BLACS_CONTEXT) {
+    local_nrows = numroc_(&nrows, &block_nrows, &MYROW, &begin_prow, &MPIGRID[0]);
+    local_ncols = numroc_(&ncols, &block_ncols, &MYCOL, &begin_pcol, &MPIGRID[1]);
+
+    int INFO;
+    DESC.resize(9);
+    descinit_(DESC.data(), &nrows, &ncols, &block_nrows, &block_ncols,
+              &begin_prow, &begin_pcol, &BLACS_CONTEXT, &local_nrows, &INFO);
+
+    data.resize((size_t)local_nrows * (size_t)local_ncols);
+  }
+
+  int glob_row(int local_row) {
+    return indxl2g(local_row + 1, DESC[3], MYROW, MPIGRID[0]) - 1;
+  }
+
+  int glob_col(int local_col) {
+    return indxl2g(local_col + 1, DESC[4], MYCOL, MPIGRID[1]) - 1;
+  }
+
+  void set_local(size_t local_row, size_t local_col, double value) {
+    data[local_row + local_col * local_nrows] = value;
+  }
+};
+
+
 int main(int argc, char* argv[]) {
   Hatrix::Context::init();
   Args opts(argc, argv);
   int N = opts.N;
-
+  int BLACS_CONTEXT;
 
   {
     int provided;
@@ -31,6 +75,10 @@ int main(int argc, char* argv[]) {
   MPI_Comm_size(MPI_COMM_WORLD, &MPISIZE);
   MPI_Comm_rank(MPI_COMM_WORLD, &MPIRANK);
   MPIGRID[0] = MPISIZE; MPIGRID[1] = 1;
+
+  Cblacs_get(-1, 0, &BLACS_CONTEXT );
+  Cblacs_gridinit(&BLACS_CONTEXT, "Row", MPIGRID[0], MPIGRID[1]);
+  Cblacs_pcoord(BLACS_CONTEXT, MPIRANK, &MYROW, &MYCOL);
 
   // Init domain decomposition for H2 matrix using DTT.
   auto start_domain = std::chrono::system_clock::now();
@@ -53,15 +101,20 @@ int main(int argc, char* argv[]) {
   SymmetricSharedBasisMatrix A;
 
   auto start_construct = std::chrono::system_clock::now();
-
-  Cblacs_get(-1, 0, &BLACS_CONTEXT );
-  Cblacs_gridinit(&BLACS_CONTEXT, "Row", MPIGRID[0], MPIGRID[1]);
-  Cblacs_pcoord(BLACS_CONTEXT, MPIRANK, &MYROW, &MYCOL);
   if (opts.admis_kind == GEOMETRY) {
     init_geometry_admis(A, domain, opts); // init admissiblity conditions with DTT
   }
   else if (opts.admis_kind == DIAGONAL) {
     init_diagonal_admis(A, domain, opts); // init admissiblity conditions with diagonal condition.
+  }
+
+  ScaLAPACK_dist_matrix_t DENSE(N, N, SCALAPACK_BLOCK_SIZE, SCALAPACK_BLOCK_SIZE, 0, 0, BLACS_CONTEXT);
+  for (size_t i = 0; i < DENSE.local_nrows; ++i) {
+    for (size_t j = 0; j < DENSE.local_ncols; ++j) {
+      double value = opts.kernel(domain.particles[DENSE.glob_row(i)].coords,
+                                 domain.particles[DENSE.glob_col(i)].coords);
+      DENSE.set_local(i, j, value);
+    }
   }
 
 
