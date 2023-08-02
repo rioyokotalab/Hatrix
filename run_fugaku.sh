08840ebc9cb6dc596da1262426d40a8dd5e927ee
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2023-08-02 19:39:23 +0900

    update fugaku script

--- run_fugaku.sh
+++ run_fugaku.sh
@@ -3,10 +3,6 @@
 #PJM -L "rscunit=rscunit_ft01"
 #PJM -L "rscgrp=small"
 #PJM -L "elapse=24:00:00"
-#PJM -L "freq=2200"
-#PJM -L "throttling_state=0"
-#PJM -L "issue_state=0"
-#PJM -L "ex_pipe_state=0"
 #PJM --mpi "proc=128"
 #PJM --mpi "max-proc-per-node=1"
 #PJM -s
